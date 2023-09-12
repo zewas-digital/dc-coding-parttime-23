c5b87cb57a692bb92c013f200b70b525c6cf8fd2
Jeremias <jeremias@zewas.at>
2023-09-12 21:45:10 +0200

    added Helllo World

--- examples/week1/example1/src/at/digitalcampus/example2/HelloWorld2.java
+++ examples/week1/example1/src/at/digitalcampus/example2/HelloWorld2.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+package at.digitalcampus.example2;
+
+public class HelloWorld2 {
+    public void test() {
+        System.out.println("Hello World1234!");
     }
 }
