7534c341901e48ebdbc847760a9adc279e36a541
finkf <@>
2023-09-23 14:22:50 +0200

    sumofdigits beendet

--- src/solutions/florian/weekend1/example22/SumOfDigits.java
+++ src/solutions/florian/weekend1/example22/SumOfDigits.java
@@ -6,20 +6,37 @@ package solutions.florian.weekend1.example22;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-        int startwert= 10;
-        int endwert = 20;
-        int summe = 0;
-        int zahl1 = 0;
+        int ziffernsumme = calculateSumofDigits(245);
+        int ziffernsumme2 = calculateSumofDigit(245);
+        System.out.println(ziffernsumme);
+        System.out.println(ziffernsumme2);
+    }
 
-        for (int i = startwert; i < endwert ; i++) {
-            summe += i;
-            System.out.println(summe);
+    static int calculateSumofDigits(int number) {
+        //245
+
+        int ergebnis = 0;
+
+        while (number > 0) {
+            int ziffer = number % 10; // 5; 4; 2;
+            ergebnis = ergebnis + ziffer; // 0 + 5; 5 + 4; 9 + 2
+            number = number / 10;
         }
-        System.out.println(summe / 100);
-        //if (true)
-        System.out.println(summe % 100);
-        summe = zahl1;
-        System.out.println(zahl1);
 
+        return ergebnis;
+    }
+    static int calculateSumofDigit(int number) {
+        //245
+
+        int ergebnis = 0;
+
+        for (int i = 0; number > 0; i++) {
+
+            int ziffer = number % 10; // 5; 4; 2;
+            ergebnis = ergebnis + ziffer; // 0 + 5; 5 + 4; 9 + 2
+            number = number / 10;
+        }
+
+        return ergebnis;
     }
 }
--- src/solutions/florian/weekend1/example23/Fibonacci.java
+++ src/solutions/florian/weekend1/example23/Fibonacci.java
@@ -1,9 +1,11 @@
 /*
 Aufgabe:
-Schreiben Sie ein Java-Programm, das die n-te Zahl in der Fibonacci-Folge berechnet, ohne die Verwendung von Arrays oder anderen Datenstrukturen außerhalb der Verwendung von Variablen.
+Schreiben Sie ein Java-Programm, das die n-te Zahl in der Fibonacci-Folge berechnet, ohne die Verwendung von Arrays oder
+anderen Datenstrukturen außerhalb der Verwendung von Variablen.
 
 Erläuterung:
-    Das Programm sollte eine Methode fibonacci(int n) enthalten, die als Eingabe eine nicht negative ganze Zahl n erhält und die n-te Zahl in der Fibonacci-Folge zurückgibt.
+    Das Programm sollte eine Methode fibonacci(int n) enthalten, die als Eingabe eine nicht negative ganze Zahl n erhält
+    und die n-te Zahl in der Fibonacci-Folge zurückgibt.
     Achten Sie darauf, den Basisfall zu berücksichtigen: F(0) = 0 und F(1) = 1.
  */
 
@@ -11,6 +13,23 @@ package solutions.florian.weekend1.example23;
 
 public class Fibonacci {
     public static void main(String[] args) {
-
+        int ergebnis = fibonacci(10);
+        System.out.println(ergebnis);
     }
+
+    static int fibonacci (int n){
+        int länge = 20;
+        int f = 0;
+        int f2 = 1;
+        int ergebnis = 0;
+
+        for (int i = 0; i < länge ; i++) {
+
+
+        }
+
+    return ergebnis;
+    }
+
+
 }
