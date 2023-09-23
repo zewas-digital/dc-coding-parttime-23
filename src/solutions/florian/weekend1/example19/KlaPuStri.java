424c60c9804518226aba09b9d9a959e4b9a6b928
finkf <@>
2023-09-23 12:38:25 +0200

    added examples KlaPuStri to SumOfDigits

--- src/solutions/florian/weekend1/example19/KlaPuStri.java
+++ src/solutions/florian/weekend1/example19/KlaPuStri.java
@@ -9,19 +9,13 @@ package solutions.florian.weekend1.example19;
 public class KlaPuStri {
     public static void main(String[] args) {
 
+       /*C4*/ System.out.println((((2+ 1.0 / 2) - (1 + 2.0 / 3)) * (6.0 / 9)) / (1.0 / 2));
+       /*C5*/ System.out.println((4+ 1.0 / 2) + (2 +1.0 / 4) * ((0.75) + (1.0 / 4) - (3.0 / 6) * (2.0 / 3) + ((4.0 / 3) - (2.0 / 5) * (10.0 / 6))));
+       /*C6*/ System.out.println((1 + 5.0 / 6) + ((5.0 / 9) + (1 + 3.0 / 7) * (14.0 / 15)) - (2 + 1.0 / 3));
+       /*C7*/ System.out.println(((6.0 / 5) / (2.0 / 5) + ((3.0 / 10) + (3.0 / 4) * ( 2.0 / 6))) - (1.0 / 5));
+       /*C8*/ System.out.println((((2.0 / 7) / (4.0 / 7) + (1.0 / 4)) * (2.0 / 3) - (10.0 / 3)) + (3.0 / 5) / (9.0 / 10));
 
-        System.out.println((2 + 1.0 / 2 - 1 + 2.0 / 3));
 
-
-
-
-
-        System.out.println((2.5 - 1.66) * 0.66 / 0.5);
-        System.out.println(4.5 + 2.25 * 0.75 + 0.25 - 0.5 * 0.6 + (1.33 - 0.4 * 1.6));
-        System.out.println(1.83 + (0.55 + 1.42 * 0.93) - 2.33);
-        System.out.println(1.2/0.4 + (0.3 + 0.75 * 0.33) - 0.2);
-        System.out.println((0.28 / 0.57 + 0.25) * 0.66 - 3.33 + 0.6 / 0.9);
-        System.out.println(1+2.0/3);
     }
 
 }
--- src/solutions/florian/weekend1/example20/Truthtable.java
+++ src/solutions/florian/weekend1/example20/Truthtable.java
@@ -5,19 +5,41 @@ Wir berechnen alle möglichen Kombinationen von Werten für A und B und zeigen d
 
 package solutions.florian.weekend1.example20;
 
+import solutions.florian.week2.example12.PrintTriangle;
+
 public class Truthtable {
 
 
     public static void main(String[] args) {
-        int a = 100;
-        int b = 200;
+        boolean a = true;
+        boolean b = false;
 
-        if (a > 100 && b < 200);{
+        if (15 % 10 == 0 && 10 + 10 == 20){
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+        }
 
+        if (20 > 30 || 40 - 10  == 30){
+            System.out.println(a);
+        }else {
+            System.out.println(b);
+        }
+
+        if ('a' == 'a') {
+            System.out.println(a);
+        }else {
+            System.out.println(b);
         }
 
 
 
 
+
     }
+
 }
+
+/*System.out.println(false && false);  beide müssen true sein */
+/*System.out.println(false || true);   eines muss true sein */
+/*System.out.println(100 == 100);      beide müssen gleich sein */
