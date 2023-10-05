ae8ed90d501993919eff354d3cb00c0af8410f92
Oliver Bedogg <ion@ions.at>
2023-10-05 16:00:59 +0200

    arrays: first 3 exercises

---
+++ src/oliver/week04/Arrays_0_Array.java
@@ -0,0 +1,7 @@
+package oliver.week04;
+
+public class Arrays_0_Array {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/oliver/week04/Arrays_3_RandomNumberArray.java
@@ -0,0 +1,17 @@
+package oliver.week04;
+
+/*
+Aufgabe: Random Number Array
+Erstelle ein int[] und befülle es mit Zufallszahlen von 0 bis 100.
+
+Random random = new Random();
+...
+int randomNumber = random.nextInt([Upper bound]);
+Gib es mit Hilfe einer foreach-Schleife aus.
+Gib es mit Hilfe einer fori-Schleife aus. Das Ziel ist eine identische Ausgabe, wie Arrays.toString()
+Gib den zweiten, fünften und zehnten Wert des Arrays aus.
+Gib jeden zweiten Wert des int[] mit Hilfe einer Schleife aus.
+ */
+
+public class Arrays_3_RandomNumberArray {
+}
