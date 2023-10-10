691725e072b6ce78b4e29f9d6846777f611ac76d
mkoenig <@>
2023-10-10 19:29:09 +0200

    Arraybeispiel 4+5 erstellt/gelöst

--- src/eslem04/Arrays_2_CopyArray.java
+++ src/eslem04/Arrays_2_CopyArray.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class Arrays_2_CopyArray {
     public static void main(String[] args) {
-        public static int[] makeACopy ( int[] original){
+        //public static int[] makeACopy ( int[] original){
 
             Scanner scanner = new Scanner(System.in);
 
@@ -31,4 +31,3 @@ public class Arrays_2_CopyArray {
 
         }
     }
-}
