dad1460d72ec310ebc7d47a4736989bf1df45f43
Oliver Bedogg <ion@ions.at>
2023-10-10 19:32:58 +0200

    added updates from michael-k

---
+++ src/michael_k/week4/Arrays_5_ArrayZaehlen.java
@@ -0,0 +1,35 @@
+package michael_k.week04;
+
+/*
+Aufgabe: Random Number Array Zählen
+Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.
+
+Erstelle eine Funktion, die die Werte größer als 30 zählt.
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Arrays_5_ArrayZaehlen {
+
+    public static void main(String[] args) {
+
+        Random randomValue = new Random ();
+        int[] array = new int[10];
+        int length = array.length;
+        int counter = 0 ;
+
+        for (int i = 0; i <length; i++) {
+            array[i] = randomValue.nextInt ( 101);
+        }
+
+        for (int value: array) {
+            if ( value > 30){ counter++;}
+        }
+
+        System.out.println ( Arrays.toString(array ) );
+        System.out.println ( );
+        System.out.println ("Es befinden sich "+counter+" Zahlen über 30 in dem Array" );
+    }
+
+}
