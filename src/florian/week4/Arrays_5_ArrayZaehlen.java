94fbb6c3cbd4c3ee46fc75fb5f957d93c92d3ed4
finkf <@>
2023-10-12 17:59:21 +0200

    Aufgabe 5 erledigt

--- src/florian/week4/Arrays_5_ArrayZaehlen.java
+++ src/florian/week4/Arrays_5_ArrayZaehlen.java
@@ -7,18 +7,22 @@ Erstelle ein int[] und befülle es mit Zufallszahlen von 1 bis 100.
 Erstelle eine Funktion, die die Werte größer als 30 zählt.
  */
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Arrays_5_ArrayZaehlen {
     public static void main(String[] args) {
         Random random = new Random();
-        int [] array = new int[20];
+        int [] array = new int[10];
 
         for (int i = 0; i < array.length; i++) {
-            array [i] = random.nextInt(1,100);
+            array[i] = random.nextInt(1, 100);
+
+            if (array[i] > 30 || array[i] >= 30){
+                System.out.print(array[i] + " ");
+            }
 
         }
-
 
 
 
