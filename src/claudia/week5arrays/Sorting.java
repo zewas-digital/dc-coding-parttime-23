cc92565231c9fa968547a6847add43c5373c5bc0
Claudia <@>
2023-10-14 17:59:49 +0200

    Mergesort

--- src/claudia/week5arrays/Sorting.java
+++ src/claudia/week5arrays/Sorting.java
@@ -11,17 +11,76 @@ public class Sorting {
         for (int i = 0; i < vector1.length; i++) {
             vector1[i] = random.nextInt(1, 101);
         }
-
+        //TESTWERTE:
+        //int [] a = {1, 3, 5};
+        //int [] a = {8};
+        //int [] b = {2, 4, 6, 8, 10};
+        //int [] a = {2, 2, 2, 2, 2};
+        //int [] b = {1};
         //int [] vector1 = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,};
-        //int [] vector1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //int [] vector1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+        //int [] vector = {1, 2, 3, 7, 2, 4, 5, 5, 6, 100, 234};
+        int[] vector = {2,4, 1, 3};
+        //mergeArray(vector, 0, 3, 10);
+        mergeSort(vector,0, 3);
+        System.out.println(Arrays.toString(vector));
+        /*
         int [] vector2 = Arrays.copyOf(vector1, vector1.length);
         System.out.println("Originalvektor: ");
         System.out.println(Arrays.toString(vector1));
-        System.out.println(Arrays.toString(cocktailSortAscending(vector1)));
-        System.out.println(Arrays.toString(bubbleSortAscending(vector2)));
+        cocktailSortAscending(vector1);
+        System.out.println(Arrays.toString(vector1));
+        bubbleSortAscending(vector2);
+        System.out.println(Arrays.toString(vector2));*/
     }
 
-    public static int[] cocktailSortAscending(int[] vector) {
+    public static void mergeSort(int[] vector, int lowest, int highest){
+        boolean sorted = false;
+        //while (!sorted){
+
+        //while not sorted, divide array into approximate halves,
+        //lower part: index 0 to (0,5 * (length - 1)) ! /2 by integer-division!
+        //upper part: (0,5 * (length - 1) + 1) to (length - 1) ! /2 by integer-division!
+        //until length of array is 1 => lowest and highest index == 0
+        //subarray of length 1 is sorted by definition
+        //merge sorted arrays recursively
+            if (lowest < highest) {
+                int middle = (highest - lowest) / 2;
+                mergeSort(vector, lowest, middle);
+                mergeSort(vector, middle + 1, highest);
+                mergeArray(vector, lowest, middle, highest);
+            } //else sorted = true;
+        }
+    //}
+
+    public static void mergeArray (int[] vector, int lowest, int middle, int highest){
+        //merge two sorted subarrays into one (sorted) array
+        //subarrays must be sorted ascendingly
+        System.out.println("Merge!");
+        int[] sorted = new int[vector.length]; //array to store sorted values temporarily
+        int indexA = 0;
+        int indexB = middle + 1;
+        for (int i = 0; i < vector.length; i++) {
+            if (indexA <= middle && indexB <= highest && vector[indexA] <= vector[indexB]) {
+                sorted[i] = vector[indexA];
+                indexA++;
+            } else if (indexA <= middle && indexB <= highest && vector[indexA] > vector[indexB]) {
+                sorted[i] = vector[indexB];
+                indexB++;
+            } else if (indexA > middle) {
+                sorted[i] = vector[indexB];
+                indexB++;
+            } else {
+                sorted[i] = vector[indexA];
+                indexA++;
+            }
+        }
+        System.arraycopy(sorted, 0, vector, 0, vector.length);
+    }
+
+
+
+    public static void cocktailSortAscending(int[] vector) {
         int counterLoop = 0;
         int counterSwap = 0;
         boolean swapped = true;
@@ -52,11 +111,9 @@ public class Sorting {
         }
         System.out.println("Cocktailsort, Anzahl Ausführung Schleife " + counterLoop);
         System.out.println("Cocktailsort, Anzahl vorgenommene Swaps: " + counterSwap);
-
-        return vector;
     }
 
-    public static int[] bubbleSortAscending(int[] vector) {
+    public static void bubbleSortAscending(int[] vector) {
         int counterLoop = 0;
         int counterSwap = 0;
         for (int j = 0; j < vector.length - 1; j++) {
@@ -70,8 +127,6 @@ public class Sorting {
         }
         System.out.println("Bubblesort, Anzahl Ausführung Schleife: " + counterLoop);
         System.out.println("Bubblesort, Anzahl vorgenommene Swaps: " + counterSwap);
-
-        return vector;
     }
 
     public static void swapElements(int [] vector, int i, int j){
