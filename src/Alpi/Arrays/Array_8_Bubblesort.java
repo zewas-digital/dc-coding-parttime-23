b835bcc876c7e93ff540a8b5a23b4f5a1d386294
Alpi <@>
2023-10-17 17:27:03 +0200

    test

---
+++ src/Alpi/Arrays/Array_8_Bubblesort.java
@@ -0,0 +1,13 @@
+package Alpi.Arrays;
+
+/*
+Erstelle ein int[] und sortiere es mit einem selbsterstellten Bubblesort Algorithmus und gib es dann aus. Der Bubblesort Algorithmus soll in einer eigenen Methode implementiert werden. Der Aufruf und die Ausgabe erfolgt in der main Methode.
+
+    Sortiere der Größe nach aufsteigend (Ascending)
+    Sortiere der Größe nach absteigend (Descending)
+
+Erklärung Bubblesort: https://www.youtube.com/watch?v=qtXb0QnOceY
+ */
+
+public class Array_8_Bubblesort {
+}
