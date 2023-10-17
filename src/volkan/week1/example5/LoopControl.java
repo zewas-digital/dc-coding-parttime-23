58facae0a4fd83557c767334860ca7c6fbd33061
volkanmeral <@>
2023-10-17 18:35:54 +0200

    test

--- src/volkan/week1/example5/LoopControl.java
+++ src/volkan/week1/example5/LoopControl.java
@@ -3,8 +3,7 @@ package volkan.week1.example5;
 /*
 Schreibe ein Programm das im Bereich von 1-100 Zahlen die durch zwei und/oder durch drei teilbar sind ausgeben.
 Beende die Schleife, wenn 20 Zahlen ausgegeben wurden dann mit der break; Anweisung.
-
- */
+*/
 
 public class LoopControl {
 
