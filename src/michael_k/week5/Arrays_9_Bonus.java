6c8ae0631808861ae026ce35b0a5e6ed2e28340a
mkoenig <@>
2023-10-19 21:45:41 +0200

    Radixsort hinzugefügt

--- src/michael_k/week5/Arrays_9_Bonus.java
+++ src/michael_k/week5/Arrays_9_Bonus.java
@@ -10,25 +10,91 @@ Den Pseudo-Code der Algorithmen findest du unter:
 https://panthema.net/2013/sound-of-sorting/SoS-CheatSheet.pdf
  */
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Arrays;
 
 public class Arrays_9_Bonus {
 
     public static void main(String[] args) {
 
 
-        int[] array = genarateRandomArray ( 5 );
+        int[] array = genarateRandomArray ( 8 );
 
         System.out.println ( Arrays.toString ( array ) );
-        System.out.println ( Arrays.toString ( bubbleSortminmax ( array, array.length ) ));
-        System.out.println (  Arrays.toString ( gnomesort ( array, array.length ) ));
+        //System.out.println ( Arrays.toString ( bubbleSortminmax ( array, array.length ) ));
+        //System.out.println (  Arrays.toString ( gnomesort ( array, array.length ) ));
+        System.out.println ( radixsort ( array, array.length ));
 
 
     }
 
 
-  //static int[] quicksort(int array, );
+    static ArrayList<Integer> radixsort(int[] array, int length){
+
+        ArrayList<Integer> sortArray = new ArrayList<>();
+        // array in eine list kopieren
+        for (int i = 0; i < array.length; i++) {
+            sortArray.add( array[i] );
+        }
+
+        ArrayList<Integer> list0 = new ArrayList<> ( );
+        ArrayList<Integer> list1 = new ArrayList<> ( );
+        ArrayList<Integer> list2 = new ArrayList<> ( );
+        ArrayList<Integer> list3 = new ArrayList<> ( );
+        ArrayList<Integer> list4 = new ArrayList<> ( );
+        ArrayList<Integer> list5 = new ArrayList<> ( );
+        ArrayList<Integer> list6 = new ArrayList<> ( );
+        ArrayList<Integer> list7 = new ArrayList<> ( );
+        ArrayList<Integer> list8 = new ArrayList<> ( );
+        ArrayList<Integer> list9 = new ArrayList<> ( );
+
+        for (int i = 1; i < 4; i++) {
+
+            for (int j = 0; j < length; j++) {
+
+                int x = zifferVonStelle ( sortArray.get (j), i );
+
+                if (x == 0) {list0.add ( sortArray.get (j) );}
+                if (x == 1) {list1.add ( sortArray.get (j) );}
+                if (x == 2) {list2.add ( sortArray.get (j) );}
+                if (x == 3) {list3.add ( sortArray.get (j) );}
+                if (x == 4) {list4.add ( sortArray.get (j) );}
+                if (x == 5) {list5.add ( sortArray.get (j) );}
+                if (x == 6) {list6.add ( sortArray.get (j) );}
+                if (x == 7) {list7.add ( sortArray.get (j) );}
+                if (x == 8) {list8.add ( sortArray.get (j) );}
+                if (x == 9) {list9.add ( sortArray.get (j) );}
+            }
+
+            sortArray.clear ( );
+            sortArray.addAll ( list0 );
+            sortArray.addAll ( list1 );
+            sortArray.addAll ( list2 );
+            sortArray.addAll ( list3 );
+            sortArray.addAll ( list4 );
+            sortArray.addAll ( list5 );
+            sortArray.addAll ( list6 );
+            sortArray.addAll ( list7 );
+            sortArray.addAll ( list8 );
+            sortArray.addAll ( list9 );
+            list0.clear ();
+            list1.clear ();
+            list2.clear ();
+            list3.clear ();
+            list4.clear ();
+            list5.clear ();
+            list6.clear ();
+            list7.clear ();
+            list8.clear ();
+            list9.clear ();
+
+        }
+
+
+
+        return sortArray;
+    }
     static int[] gnomesort(int[] array, int length){
 
         int[] sortArray =new int[length];
@@ -69,17 +135,18 @@ public class Arrays_9_Bonus {
             arraySort[i] = array[i];
         }
 
-        while(counter != length-1){
-            counter =0;
-            for (int i = 0; i <length-1 ; i++) {
+        for (int i = 0; i < length -1; i++) {
 
-                if (arraySort[i] > arraySort[i+1]){
-                    x = arraySort[i];
-                    y = arraySort[i+1];
+            for (int j = 0; j <length - i - 1 ; j++) {
 
-                    arraySort[i] = y;
-                    arraySort[i+1] = x;
-                }else{ counter++;}
+                if (arraySort[j] > arraySort[j+1]){
+                    x = arraySort[j];
+                    y = arraySort[j+1];
+
+                    arraySort[j] = y;
+                    arraySort[j+1] = x;
+                }
+
             }
 
         }
@@ -92,11 +159,21 @@ public class Arrays_9_Bonus {
         Random randomnumber = new Random();
 
         for (int i = 0; i <length; i++) {
-            int value =randomnumber.nextInt ( 100 );
+            int value =randomnumber.nextInt ( 101,1000 );
             array[i] = value +1;
         }
 
         return array;
     }
+    static int zifferVonStelle (int value, int stelle){
+        int ziffer =0;
 
+        for (int i = 1; i < stelle; i++) {
+            value = value / 10;
+        }
+
+        ziffer = value % 10;
+
+        return ziffer;
+    }
 }
