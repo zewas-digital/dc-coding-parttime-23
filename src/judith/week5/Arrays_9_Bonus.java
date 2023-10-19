9c6043fd9db85c7e0752c688938fa275e1d94851
DCV <tests@>
2023-10-19 21:41:45 +0200

    changes 19.10.2023 Arrays

--- src/judith/week5/Arrays_9_Bonus.java
+++ src/judith/week5/Arrays_9_Bonus.java
@@ -10,5 +10,32 @@ Den Pseudo-Code der Algorithmen findest du unter:
 https://panthema.net/2013/sound-of-sorting/SoS-CheatSheet.pdf
  */
 
+import java.util.Arrays;
+
 public class Arrays_9_Bonus {
+    public static void main(String[] args) {
+        int[] unsortiert  = {24, 7, 12, 25, 64};
+       // int[] sortiert = insertionSort(unsortiert);
+
+    }
+  /*  public static int[] insertionSort (int[] unsortiert ){
+
+        for (int i = 0; i < unsortiert.length - 1 ; i++) {       // Anzahl der Durchläufe
+            System.out.println(" Durchlauf i: " + i);
+            int j = i;
+            int sortiert = 0;
+            while (j > 0 & unsortiert[j - 1]){
+                unsortiert[j] = unsortiert[j - 1];
+                System.out.println(Arrays.toString(unsortiert[j]));
+            }
+
+        }
+        return unsortiert;
+
+         */
+
+
+
+
+    }
 }
