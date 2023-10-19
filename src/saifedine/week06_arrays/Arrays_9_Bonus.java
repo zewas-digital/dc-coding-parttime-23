2ad237b6e2f95e2d4b77d1001b81aa48d227ae1c
easb2 <12345@>
2023-10-19 21:31:15 +0200

    19.10.23

--- src/saifedine/week06_arrays/Arrays_9_Bonus.java
+++ src/saifedine/week06_arrays/Arrays_9_Bonus.java
@@ -10,5 +10,42 @@ Den Pseudo-Code der Algorithmen findest du unter:
 https://panthema.net/2013/sound-of-sorting/SoS-CheatSheet.pdf
  */
 
+import saifedine.week05_arrays.Array_7_MinMaxAvg;
+
+import java.util.Arrays;
+
 public class Arrays_9_Bonus {
+
+    public static void main(String[] args) {
+
+        int[] elements = Array_7_MinMaxAvg.getZufallsZahlen();
+        int[] sortiert = sort(elements);
+
+    }
+
+
+    static int[] sort(int[] elements) {
+
+        for (int i = 1; i < elements.length; i++) {
+
+            int elementToSort = elements[i];
+
+            // Move element to the left until it's at the right position
+            int j = i;
+
+            while (j > 0 && elementToSort < elements[j - 1]) {
+                elements[j] = elements[j - 1];
+                j--;
+            }
+            elements[j] = elementToSort;
+        }
+        System.out.println("Aufgabe - Insertion Sort:");
+
+        System.out.println("Ergebnis: " + Arrays.toString(elements));
+
+        return elements;
+    }
+
+
 }
+
