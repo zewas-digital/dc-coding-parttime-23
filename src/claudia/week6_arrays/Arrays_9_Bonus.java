6281a96dfa77402fc09aebf64447c63192c312f9
Claudia <@>
2023-10-19 18:01:05 +0200

    Added Quicksort

--- src/claudia/week6_arrays/Arrays_9_Bonus.java
+++ src/claudia/week6_arrays/Arrays_9_Bonus.java
@@ -21,46 +21,69 @@ public class Arrays_9_Bonus {
         int[] vector1 = createRandomArray(50);
         int[] vector2 = Arrays.copyOf(vector1, vector1.length);
         int[] vector3 = Arrays.copyOf(vector1, vector1.length);
+        int[] vector4 = Arrays.copyOf(vector1, vector1.length);
 
         System.out.println("Originalvektor: ");
         System.out.println(Arrays.toString(vector1));
         cocktailSortAscending(vector1);
+        System.out.println("Cocktailsort: ");
         System.out.println(Arrays.toString(vector1));
         bubbleSortAscending(vector2);
+        System.out.println("Bubblesort: ");
         System.out.println(Arrays.toString(vector2));
         insertionSortAscending(vector3);
+        System.out.println("Insertionsort: ");
         System.out.println(Arrays.toString(vector3));
+        quicksort(vector4);
+        System.out.println("Quicksort: ");
+        System.out.println(Arrays.toString(vector4));
     }
 
 
+    public static void quicksort(int[] vector) {
+        qsort(vector, 0, vector.length - 1);
+    }
+
+    public static void qsort(int[] vector, int lowest, int highest) {
+        if (lowest < highest) {//=> vector.length > 1
+            int indexPivot = partition(vector, lowest, highest); //then pivot at position indexPivot
+            qsort(vector, lowest, indexPivot - 1);
+            qsort(vector, indexPivot + 1, highest);
+        }
+
+    }
+
+    public static int partition(int[] vector, int lowest, int highest) {
+        int pivot = vector[highest]; //TODO improve choice of pivot
+        int i = lowest;
+        for (int j = lowest; j < highest; j++) {
+            if (vector[j] < pivot) {
+                swapElements(vector, i, j);
+                i++; //i is only incremented if vector[j] < pivot!
+            }
+        }
+        swapElements(vector, i, highest);//pivot is moved into its final position
+        return i;
+    }
     public static void insertionSortAscending(int[] vector) {
-        int counterLoop = 0;
-        int counterSwap = 0;
         for (int i = 1; i < vector.length; i++) {
             int valueToBeInserted = vector[i];
             int j = i;
             while (j > 0 && vector[j - 1] > valueToBeInserted) {
-                counterSwap++;
                 vector[j] = vector[j - 1];
                 j--;
             }
             vector[j] = valueToBeInserted;
         }
-        System.out.println("Insertionsort, Anzahl vorgenommene Swaps: " + counterSwap);
     }
 
     public static void cocktailSortAscending(int[] vector) {
-        int counterLoop = 0;
-        int counterSwap = 0;
         boolean swapped = true;
-
         while (swapped) {
             swapped = false;
             for (int i = 0; i < vector.length - 1; i++) {
-                counterLoop++;
                 if (vector[i] > vector[i + 1]) {
                     swapElements(vector, i, i + 1);
-                    counterSwap++;
                     swapped = true;
                 }
             }
@@ -68,34 +91,23 @@ public class Arrays_9_Bonus {
             if (swapped) { //wenn oben nichts mehr geändert wurde, sind wir fertig
                 swapped = false;
                 for (int i = vector.length - 1; i > 0; i--) {
-                    counterLoop++;
                     if (vector[i] < vector[i - 1]) {
                         swapElements(vector, i, i - 1);
-                        counterSwap++;
                         swapped = true;
                     }
                 }
-                //System.out.println("nach zweiter For-Schleife " + Arrays.toString(vector));
             }
         }
-        System.out.println("Cocktailsort, Anzahl Ausführung Schleife " + counterLoop);
-        System.out.println("Cocktailsort, Anzahl vorgenommene Swaps: " + counterSwap);
     }
 
     public static void bubbleSortAscending(int[] vector) {
-        int counterLoop = 0;
-        int counterSwap = 0;
         for (int j = 0; j < vector.length - 1; j++) {
             for (int i = 0; i < vector.length - j - 1; i++) {
-                counterLoop++;
                 if (vector[i] > vector[i + 1]) {
                     swapElements(vector, i, i + 1);
-                    counterSwap++;
                 }
             }
         }
-        System.out.println("Bubblesort, Anzahl Ausführung Schleife: " + counterLoop);
-        System.out.println("Bubblesort, Anzahl vorgenommene Swaps: " + counterSwap);
     }
 
     public static void swapElements(int[] vector, int i, int j) {
@@ -104,7 +116,7 @@ public class Arrays_9_Bonus {
         vector[j] = temp;
     }
 
-    private static int[] createRandomArray(int size) {
+    public static int[] createRandomArray(int size) {
         Random random = new Random();
         int[] vector = new int[size];
         for (int i = 0; i < size; i++) {
