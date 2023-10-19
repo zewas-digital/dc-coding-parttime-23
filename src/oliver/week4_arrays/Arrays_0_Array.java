66fba42d8f8f19a3d35ca3a5f98053b56d132b46
Oliver Bedogg <ion@ions.at>
2023-10-19 19:19:12 +0200

    Array Bubble Sort and Method Call from another Class

--- src/oliver/week4_arrays/Arrays_0_Array.java
+++ src/oliver/week4_arrays/Arrays_0_Array.java
@@ -9,6 +9,7 @@ public class Arrays_0_Array {
         int[] numbers = new int[length];
         String[] names = new String[length];
 
+        // array-felder befüllen
         numbers[0] = length;
         numbers[3] = 7;
         names[7] = "Hugo";
@@ -18,9 +19,10 @@ public class Arrays_0_Array {
         System.out.println(names[7]);
         System.out.println("länge von names: "+ names.length);
 
-        // array mit for-each schleife
+        // statisches array generieren
         int[] values = {10, 50, 60, 80, 90, 100};
 
+        // array mit for-each schleife
         for (int i = 0; i < values.length; i++) {
             System.out.println("i: " + i + " = " + values[i] + " ");
         }
