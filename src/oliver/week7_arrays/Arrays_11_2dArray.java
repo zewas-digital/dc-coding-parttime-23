df79bf48f6484ceba3123e058b021d9c12db9396
Oliver Bedogg <ion@ions.at>
2023-10-24 21:12:16 +0200

    static 2D array and exercise 11

---
+++ src/oliver/week7_arrays/Arrays_11_2dArray.java
@@ -0,0 +1,13 @@
+package oliver.week7_arrays;
+
+/*
+Aufgabe: 2D-Array
+
+Erstelle ein Zwei-Dimension-Array int[][] und befülle es mit Zufallszahlen von 0 bis 100.
+Frage dazu die Längen der Dimensionen vom Anwender ab.
+Gib die Summe pro Zeile oder pro Spalte aus.
+ */
+
+public class Arrays_11_2dArray {
+
+}
