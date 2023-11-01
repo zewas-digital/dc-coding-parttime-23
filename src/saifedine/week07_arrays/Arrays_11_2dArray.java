b10c8e087358b71ad6cf62bdb3771eee6a474061
easb2 <12345@>
2023-11-01 17:06:51 +0100

    01.11.23

--- src/saifedine/week07_arrays/Arrays_11_2dArray.java
+++ src/saifedine/week07_arrays/Arrays_11_2dArray.java
@@ -8,6 +8,39 @@ Frage dazu die Längen der Dimensionen vom Anwender ab.
 Gib die Summe pro Zeile oder pro Spalte aus.
  */
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Arrays_11_2dArray {
+
+    public static void main(String[] args) {
+
+        // Zwei-Dimension-Array --> int[][]
+        // Zufallszahlen von 0 bis 100 --> Random
+        // Abfrage Dimensionen vom Anwender --> Scanner
+        // Summer pro Zeile
+        // Summer pro Spalte
+
+        Scanner Eingabe = new Scanner(System.in);
+        Random Zufall = new Random();
+
+
+        System.out.println("Geben Sie die Länge der xDimension: ");
+        int xDimension = Eingabe.nextInt();
+
+        System.out.println("Geben Sie die Länge der yDimension: ");
+        int yDimension = Eingabe.nextInt();
+
+        int [][] numbers = new int[xDimension][yDimension];
+
+        for (int i = 0; i < xDimension; i++) {
+            for (int j = 0; j < yDimension; j++) {
+
+                numbers[i][j] = Zufall.nextInt(0, 100);
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
 
 }
