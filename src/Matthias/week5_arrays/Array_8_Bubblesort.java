eba637f8a953a89d135153d85587e3b3c2ffa5fe
matth <@>
2023-11-02 22:09:22 +0100

    02.11.2023 Array_8_Bubblesort.java added finished Version, Arrays_11_2dArray started not finished, Methods_1_calculatePrice.java no Idea What to do?

--- src/Matthias/week5_arrays/Array_8_Bubblesort.java
+++ src/Matthias/week5_arrays/Array_8_Bubblesort.java
@@ -14,14 +14,28 @@ import Matthias.week6_arrays.Methods;
 import java.util.Arrays;
 
 public class Array_8_Bubblesort {
-    public static void main(String[] args) {
-        int[] numbers = Methods.createRandomArray(10);
-        System.out.println("initial array: "+ Arrays.toString(numbers));
 
+    public static void main( String[] args) {
+
+        int[] numbers = Methods.createRandomArray(10);
+        System.out.println("Random Array 1:");
+        System.out.println( Arrays.toString( numbers ) + ";");
+        System.out.println("initial array: "+ Arrays.toString(numbers));
         bubbleSort(numbers);
         System.out.println("sorted array: "+ Arrays.toString(numbers));
+
+
+        // Matthias BubbleSort:
+        System.out.println("Random Array 2:");
+        int[] number1= Methods.createRandomArray(10);
+        System.out.println( "Array mit 10 Random Zahlen:" + Arrays.toString( number1  ) + ";");
+        bubbleSort1( number1 );
+        System.out.println("Sortierter Array:"+ Arrays.toString(number1));
+        System.out.println("Ende des Sortiervorgangs!");
     }
 
+
+    //Methode 1: Oliver
     static void bubbleSort(int[] arr){
         System.out.println("+ bubble sort ------");
 
@@ -42,5 +56,22 @@ public class Array_8_Bubblesort {
             System.out.println("+ "+ Arrays.toString(arr));
         }
         System.out.println("+ end bubble sort ------");
+    }
+    //Methode 2: Matthias
+    static void bubbleSort1(int[] arr) {
+        // 1. Bubblephase Prüfen der Arrays 9,4 - 4,9
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
