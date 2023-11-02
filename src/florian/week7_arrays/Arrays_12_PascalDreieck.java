c706f9082cd7043858c8324b19c121ac6df55e71
finkf <@>
2023-11-02 21:51:10 +0100

    Aufgabe 12 Pascalsche dreieck beendet Aufgabe 14 Tictactoe begonnen, Spielbrett fertig

--- src/florian/week7_arrays/Arrays_12_PascalDreieck.java
+++ src/florian/week7_arrays/Arrays_12_PascalDreieck.java
@@ -8,16 +8,29 @@ Regel: Jede Zahl in der Matrix ist gleich die Summe der Nachbarzahlen nach Links
 Beispiel-Bild in Slack
  */
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Arrays_12_PascalDreieck {
     public static void main(String[] args) {
-        Random random = new Random();
+
         Scanner userinput = new Scanner(System.in);
-        System.out.println("Gib die Spalten anzahl des Arrays ein: ");
-        int columns = userinput.nextInt();
         System.out.println("Gib die Zeilen anzahl ein: ");
+        int lines = userinput.nextInt(); // Gibt die Zeilenlänge an
+        System.out.println("Gib die Spalten anzahl des Arrays ein: ");
+        int columns = userinput.nextInt(); // Gibt die Spaltenlänge ein
+        int [][] array = new int [lines][columns];
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (i == 0 || j == 0){
+                    array [i][j] = 1;
+                } else { array[i][j] = array[i][j - 1] + array [i - 1][j];
+                }
+            }
+            System.out.println(Arrays.toString(array[i]));
+        }
 
 
     }
---
+++ src/florian/week8_arrays/Array_14_TicTacToe.java
@@ -0,0 +1,51 @@
+package florian.week8_arrays;
+/*
+Programmiere das Spiel TicTacToe.
+
+Für das Spielfeld, verwende int[3][3] Array, in dem
+
+    0 = Leer
+    1 = Spieler 1
+    2 = Spieler 2
+
+Das Kriterium für Gewinn ist, 3 Zeichen in eine Reihe, Spalte oder Diagonal.
+ */
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array_14_TicTacToe {
+    public static void main(String[] args) {
+        Scanner userinput = new Scanner(System.in);
+
+        int[][] field = new int[3][3];
+        int leer = 0;
+        int player1 = 1;
+        int player2 = 2;
+
+
+
+        // Spielfeld
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (j == field[i].length - 1) {
+                    System.out.print(field[i][j]);
+                } else {
+                    System.out.print(field[i][j] + " | ");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
