1809b268ddacb54c140dc96deebf4a7f36e7d2cc
easb2 <12345@>
2023-11-04 19:46:58 +0100

    04.11.23

--- src/saifedine/array/Arrays_BS.java
+++ src/saifedine/array/Arrays_BS.java
@@ -1,4 +1,21 @@
 package saifedine.array;
 
+import java.util.Arrays;
+
 public class Arrays_BS {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        //statisches Array generieren
+
+        numbers[0] = 4;
+        numbers[1] = 7;
+        numbers[2] = 14;
+
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
 }
