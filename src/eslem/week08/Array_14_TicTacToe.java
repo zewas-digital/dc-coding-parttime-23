6b3bc0ea14c73a4626e238b6e207ccc23f822d3c
Lenovo <@>
2023-11-09 19:38:19 +0100

    changes

--- src/eslem/week08/Array_14_TicTacToe.java
+++ src/eslem/week08/Array_14_TicTacToe.java
@@ -10,44 +10,41 @@ Für das Spielfeld, verwende int[3][3] Array, in dem
 
 Das Kriterium für Gewinn ist, 3 Zeichen in eine Reihe, Spalte oder Diagonal.
  */
-<<<<<<< HEAD:src/eslem/week08/Array_14_TicTacToe.java
+import java.util.Scanner;
+import java.util.Random;
+
 
 public class Array_14_TicTacToe {
 
-        public static void main (String[]args){
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                Random random = new Random();
 
-=======
-/*public class Array_14_TicTacToe {
-    public static void main(String[] args) {
->>>>>>> origin/master:src/michael_k/eslem/week08/Array_14_TicTacToe.java
-        Scanner sc = new Scanner(System.in);
-        Random random = new Random();
+                int spieler1 = 1;
+                int spieler2 = 2;
 
-        int spieler1 = 1;
-        int spieler2 = 2;
+                boolean win = false;
 
-        boolean win = false;
+                char[][] board = new char[3][3];
 
-        char[][] board = new char[3][3];
+                //Spielfeld
 
-        //Spielfeld
+                String[][] gameBoard = {
+                        {" " + "|" + " " + "|" + " " + " "},
+                        {"-" + "-" + "-" + "-" + "-"},
+                        {" " + "|" + " " + "|" + " " + " "},
+                        {"-" + "-" + "-" + "-" + "-"},
+                        {" " + "|" + " " + "|" + " " + " "},
+                };
 
-        String[][] gameBoard = {
-                {" " + "|" + " " + "|" + " " + " "},
-                {"-" + "-" + "-" + "-" + "-"},
-                {" " + "|" + " " + "|" + " " + " "},
-                {"-" + "-" + "-" + "-" + "-"},
-                {" " + "|" + " " + "|" + " " + " "},
-        };
+                for (int zeile = 0; zeile < gameBoard.length; zeile++) {
 
-        for (int zeile = 0; zeile < gameBoard.length; zeile++) {
+                        //  System.out.print(zeile );
 
-            //  System.out.print(zeile );
-
-            for (int spalte = 0; spalte < gameBoard[zeile].length; spalte++) {
-                System.out.print(gameBoard[zeile][spalte]);
-                System.out.println();
-            }
+                        for (int spalte = 0; spalte < gameBoard[zeile].length; spalte++) {
+                                System.out.print(gameBoard[zeile][spalte]);
+                                System.out.println();
+                        }
+                }
         }
-
-*/
+}
