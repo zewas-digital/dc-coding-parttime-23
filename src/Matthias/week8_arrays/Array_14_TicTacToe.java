69524a059331241d05b9a04d24c3fb3267751706
matth <@>
2023-11-12 17:39:53 +0100

    commited Changes in Bubblesort.java, Array_14_TicTacToe.java

--- src/Matthias/week8_arrays/Array_14_TicTacToe.java
+++ src/Matthias/week8_arrays/Array_14_TicTacToe.java
@@ -12,14 +12,29 @@ Das Kriterium für Gewinn ist, 3 Zeichen in eine Reihe, Spalte oder Diagonal.
  */
 
 public class Array_14_TicTacToe {
+    public static String[] symbol = new String[]{"     ", "  X  ", "  O  "};
     public static void main( String[] args ) {
-        int[][] tictactoe = new int[3][3];
+
+
+        /****Programm Ablaufplan****/
+        // 1. Benutzer Eingabe
+        // 2. Felderbefüllen
+        // 3. Spielfeldausgeben
+        // 4. Gewinnüberprüfen
+        // 5. Gibt es einen Gewinner
+        // 6. Spieler Wechseln
 
         System.out.println("Tictactoe, zwei Spieler, drei gleiche Zeichen in Zeile, Spalte oder Diagonale gewinnen!");
         System.out.println("Wähle Position über entsprechende Zahl: ");
         System.out.println("[1   2   3]");
         System.out.println("[4   5   6]");
         System.out.println("[7   8   9]");
+        //Spielfelder aufspannen
+        int[][] Spielfeld = new int[3][3];
+        while (true ){
+
+        }
     }
-    //
+
+
 }
