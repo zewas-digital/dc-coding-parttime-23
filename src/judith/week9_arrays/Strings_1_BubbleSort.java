1ff3f7dcc49c642a24c9ebef1d45e786029cbfbb
DCV <tests@>
2023-11-14 21:24:39 +0100

    Arrays 09.11.2023

---
+++ src/judith/week9_arrays/Strings_1_BubbleSort.java
@@ -0,0 +1,20 @@
+package judith.week9_arrays;
+
+/*
+Aufgabe: Bubblesort mit Strings
+Verwende den gegebenen String[] und sortiere es mit einem selbsterstellten Bubblesort Algorithmus und gib es dann aus.
+Sortiere zuerst nach der Länge der Namen, dann nach dem Alphabet.
+Der Bubblesort Algorithmus soll in einer eigenen Methode implementiert werden.
+Der Aufruf und die Ausgabe erfolgt in der main Methode.
+
+Sortiere die Namen nach Länge aufsteigend (Ascending)
+Sortiere die Namen nach Länge absteigend (Descending)
+Sortiere die Namen lexikographisch aufsteigend (Ascending)
+Sortiere die Namen lexikographisch absteigend (Descending)
+
+Tip.: Strings können mit der Instanz Methode compareTo der String Klasse verglichen werden: "a".compareTo("b")
+ */
+
+public class Strings_1_BubbleSort {
+    String[] names = new String[]{"Joachim", "Mert", "Eric", "Marie Christine", "Benjamin", "Sandro", "Aygün", "Hassan", "Svitlana", "Lukas", "Gyula" };
+}
