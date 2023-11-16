6e926581672595fcc4c39e8109277325fdb65528
finkf <@>
2023-11-16 20:12:13 +0100

    Strings Reverse fertig, String Randomize begonnen

--- src/florian/week10_strings/Strings_3_Randomize.java
+++ src/florian/week10_strings/Strings_3_Randomize.java
@@ -3,7 +3,8 @@ package florian.week10_strings;
 /*
 Aufgabe: Randomize
 Es soll ein Wort über die Kommandozeile eingelesen werden,
-dieses die Buchstaben des Wortes sollen zufällig vertauscht werden und anschließend ausgegeben werden. Verwende hierfür text.toCharArray() für das Aufsplitten, erstelle ein neues char[], befülle es zufällig mit den Buchstaben des Wortes, füge es mittels new String(charArray) zusammen und gib es aus.
+dieses die Buchstaben des Wortes sollen zufällig vertauscht werden und anschließend ausgegeben werden. Verwende hierfür text.toCharArray() für das Aufsplitten, erstelle ein neues char[],
+befülle es zufällig mit den Buchstaben des Wortes, füge es mittels new String(charArray) zusammen und gib es aus.
 
 Ausgabe
 
@@ -12,5 +13,13 @@ Willkommen zum Zufallsshuffle Programm
 lolHa
  */
 
+import java.util.Scanner;
+
 public class Strings_3_Randomize {
+    public static void main(String[] args) {
+        Scanner userinput = new Scanner(System.in);
+
+
+
+    }
 }
