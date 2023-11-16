7c355bc238a5b2528a100124d9e3e33f3b0fbaac
Michael Real <@>
2023-11-16 21:11:03 +0100

    string1bubblesort

--- src/MichaelReal/week10_strings/Strings_2_Reverse.java
+++ src/MichaelReal/week10_strings/Strings_2_Reverse.java
@@ -1,5 +1,7 @@
 package MichaelReal.week10_strings;
 
+import java.util.Scanner;
+
 /*
 Aufgabe: Reverse
 Es soll ein Wort über die Kommandozeile eingelesen werden,
@@ -14,5 +16,37 @@ ollaH
 
  */
 public class Strings_2_Reverse {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-}
+            System.out.println("Willkommen zum Reverseprogramm");
+
+            // Benutzer zur Eingabe des Wortes auffordern
+            System.out.print("Geben Sie ein Wort ein: ");
+            String inputWord = scanner.nextLine();
+
+            // Wort umdrehen
+            String reversedWord = reverseWord(inputWord);
+
+            // Ausgabe des umgedrehten Wortes
+            System.out.println("Umgdrehetes Wort: " + reversedWord);
+
+        }
+
+        // Methode zum Umkehren des Wortes
+        private static String reverseWord(String word) {
+            char[] charArray = word.toCharArray();
+
+            // Umkehren des charArray
+            for (int i = 0; i < charArray.length / 2; i++) {
+                char temp = charArray[i];
+                charArray[i] = charArray[charArray.length - 1 - i];
+                charArray[charArray.length - 1 - i] = temp;
+            }
+
+            // Erstellen und zurückgeben des umgedrehten Wortes
+            return new String(charArray);
+        }
+    }
+
+
