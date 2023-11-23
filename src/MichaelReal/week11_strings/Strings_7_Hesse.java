44927df3bc500d6d479cb979c3070fddab093f28
Michael Real <@>
2023-11-23 18:31:24 +0100

    hesse

--- src/MichaelReal/week11_strings/Strings_7_Hesse.java
+++ src/MichaelReal/week11_strings/Strings_7_Hesse.java
@@ -16,9 +16,33 @@ Verwende die replace() Funktion.
 import data.Texts;
 
 public class Strings_7_Hesse {
-    String text = Texts.HESSE;
-
     public static void main(String[] args) {
 
-    }
-}
+                // Annahme: Der Text ist in der Variable text gespeichert.
+                String text = Texts.HESSE;
+                String wordToSearch = "Hesse";
+                String uppercaseWord = "HESSE";
+
+                // Zähle die Vorkommen von "Hesse" im Text
+                int count = countOccurrences(text.toLowerCase(), wordToSearch.toLowerCase());
+
+                // Ersetze alle Vorkommen von "Hesse" durch "HESSE" im Text
+                String modifiedText = text.replace(wordToSearch, uppercaseWord);
+
+                // Drucke die Anzahl der Vorkommen und den modifizierten Text
+                System.out.printf("Das Wort \"%s\" kommt %d Mal vor.%n", wordToSearch, count);
+                System.out.println("Modifizierter Text:");
+                System.out.println(modifiedText);
+            }
+
+            // Methode zur Zählung der Vorkommen eines Wortes in einem Text
+            private static int countOccurrences(String text, String word) {
+                int count = 0;
+                int index = text.indexOf(word);
+                while (index != -1) {
+                    count++;
+                    index = text.indexOf(word, index + 1);
+                }
+                return count;
+            }
+        }
