07f00611d33c78e8a06a7772f72b941984a8974e
Alpi <@>
2023-11-23 17:13:50 +0100

    test

---
+++ src/Alpi/week11_strings/Strings_5_Tabelle.java
@@ -0,0 +1,17 @@
+package Alpi.week11_strings;
+
+/*
+Aufgabe: Daten tabellarisch darstellen
+Erstelle einen schönen tabellarischen Ausgabe aus den folgenden Arrays:
+
+Denke daran, dass eine Tabelle eine Kopfzeile und Trennzeichen haben kann.
+ */
+
+public class Strings_5_Tabelle {
+    String[] firstName = {"Alfonso", "Beatrix-Eleonor", "Cecil", "Daniel", "Elmar"};
+    String[] lastName = {"Klein", "Kinderdorfer", "Al Elmenar", "Schmidt", "Simma"};
+    int[] age = {40, 78, 5, 18, 81};
+    String[] place = {"Wien", "Schwarzach", "Wiener Neudorf", "Sankt Pölten", "Sankt Pölten"};
+    float[] distanceFromCapital = {0f, 654.4f, 12.457634366f, 120.0f, 119.9999f};
+
+}
