b855a4585bac135e083f55f67e27554309c6c43f
matth <@>
2023-11-24 18:31:54 +0100

    Calendar_0_WhenAndWhere Calendar_1_MyBirthday.java in Work

--- src/Matthias/week11_weekend_calender/Calendar_1_MyBirthday.java
+++ src/Matthias/week11_weekend_calender/Calendar_1_MyBirthday.java
@@ -7,7 +7,18 @@ Erstelle ein Programm, das den Anwender nach seinem Geburtstag fragt und mit Hil
  an was für einem Wochentag der Geburtstag des Anwenders war. Du kannst dafür auch die GregorianCalendar.GetDayOfWeek() Methode verwenden.
  */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Calendar_1_MyBirthday {
+    //Methoden Aufruf:
 
+    //rand aus der Klasse Random
+    public static Random rand = new Random( );
+    //scanner aus der Klasse Random
+    public static Scanner scanner = new Scanner( System.in );
+    public static void main( String[] args ) {
+        //deklaration der Variabeln im Main
 
+    }
 }
