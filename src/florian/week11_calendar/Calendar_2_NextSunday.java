a49653e5dea3ac0bf6467dd4f760406e01eb273f
finkf <@>
2023-11-24 17:15:39 +0100

    Calendar 0 Fertig

---
+++ src/florian/week11_calendar/Calendar_2_NextSunday.java
@@ -0,0 +1,12 @@
+package florian.week11_calendar;
+
+/*
+
+Aufgabe: NextSunday
+
+Erstelle eine neue GregorianCalendar Instanz und gib das Datum des nächsten Sonntags aus.
+
+ */
+
+public class Calendar_2_NextSunday {
+}
