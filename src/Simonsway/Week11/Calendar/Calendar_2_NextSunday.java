e8b2840feb6cc0a0c117a946a59c8fac23a1737a
simonsway <simon.eiler@gmail.com>
2023-11-24 17:17:43 +0100

    Refactor

---
+++ src/Simonsway/Week11/Calendar/Calendar_2_NextSunday.java
@@ -0,0 +1,12 @@
+package Simonsway.Week11.Calendar;
+
+/*
+
+Aufgabe: NextSunday
+
+Erstelle eine neue GregorianCalendar Instanz und gib das Datum des nächsten Sonntags aus.
+
+ */
+
+public class Calendar_2_NextSunday {
+}
