335bc2f0c3fad5df49f4036d13ecc19a5ce0270e
volkanmeral <@>
2023-11-25 09:14:29 +0100

    Merge remote-tracking branch 'origin/master'

--- src/volkan/week11_strings/Calendar_2_NextSunday.java
+++ src/volkan/week11_strings/Calendar_2_NextSunday.java
@@ -8,5 +8,25 @@ Erstelle eine neue GregorianCalendar Instanz und gib das Datum des nächsten Son
 
  */
 
+import java.util.Calendar;
+
 public class Calendar_2_NextSunday {
+    public static void main(String[] args) {
+
+        Calendar calendar = Calendar.getInstance();
+
+        while (calendar.get(Calendar.DAY_OF_WEEK)  != Calendar.SUNDAY){
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        int month = calendar.get(Calendar.MONTH) +1 ;
+        int year = calendar.get(Calendar.YEAR);
+
+
+        System.out.println("Nächster Sonntag ist: " + day + " . " + month + " . " + year);
+
+
+
+    }
 }
