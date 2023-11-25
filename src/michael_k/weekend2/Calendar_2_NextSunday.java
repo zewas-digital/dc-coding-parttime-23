88ee2cf29ca14af89397a127e8e4768058ba4364
mkoenig <@>
2023-11-25 09:24:40 +0100

    Calender_2 verbessert

--- src/michael_k/weekend2/Calendar_2_NextSunday.java
+++ src/michael_k/weekend2/Calendar_2_NextSunday.java
@@ -8,19 +8,28 @@ Erstelle eine neue GregorianCalendar Instanz und gib das Datum des nächsten Son
 
  */
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Calendar_2_NextSunday {
 
     public static void main(String[] args) {
 
+        //aktuelle Zeit einholen
         Calendar cal = Calendar.getInstance();
 
+        // Aktueller Wochentag und Jahrestag einholen
         int dayOfWeek = cal.get ( Calendar.DAY_OF_WEEK );
         int dayOfYear = cal.get ( Calendar.DAY_OF_YEAR );
+
+        // den Jahrestag auf den nächsten Sonntag hochrechnen
         int sunday = dayOfYear + 8- dayOfWeek;
 
+        // den errechneten Jahrestag in den Kalender schreiben
         cal.set ( Calendar.DAY_OF_YEAR, sunday );
-        System.out.println (cal.getTime () );
+
+        // Formatierung und ausgeben des gesuchten Datums
+        SimpleDateFormat days = new SimpleDateFormat ( " d.MM.yy, EEEE" );
+        System.out.println ( days.format ( cal.getTime ( ) ) );
     }
 }
