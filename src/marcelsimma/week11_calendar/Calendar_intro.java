7cf88d6f7ab8140baf0cb8534989a6a8b0974345
Marcel S <marcel.simma@zewas.at>
2023-11-25 09:13:34 +0100

    added files from code review

--- src/marcelsimma/week11_calendar/Calendar_intro.java
+++ src/marcelsimma/week11_calendar/Calendar_intro.java
@@ -35,9 +35,23 @@ public class Calendar_intro {
         System.out.println(c.getTime());
         System.out.println(c.getTimeInMillis());
 
-        c.setTimeInMillis(-1239911110);
+        System.out.println("--------------");
+
+        c.set(1900,0,1,0,0,0);
+        c.set(Calendar.YEAR, 2);
+
 
         System.out.println(c.getTime());
+        System.out.println(c.getTimeInMillis());
+
+
+        System.out.println("--------------");
+
+
+        c.setTimeInMillis(0);
+        System.out.println(c.getTime());
+        System.out.println(c.getTimeInMillis());
+
 
 
 
