5eb6ed4257901ab1af6b8cbb654bd77f9b36eff8
simonsway <simon.eiler@gmail.com>
2023-11-30 19:23:46 +0100

    Refactor

---
+++ src/Simonsway/Week12/week12_BufferedReader.java
@@ -0,0 +1,48 @@
+package Simonsway.Week12;
+
+import data.Texts;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class week12_BufferedReader {
+
+    public static void main(String[] args) {
+        getSimpleText();
+    }
+
+    public static void getSimpleText() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(
+                            Objects.requireNonNull(
+                                    Texts.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")
+                            )
+                    )
+            );
+
+            String input;
+            int lineNumber = 1;
+            while ((input = reader.readLine()) != null) {
+                System.out.println("Zeile " + lineNumber + ": " + input);
+
+
+                lineNumber++;
+            }
+
+        } catch (IOException exc) {
+            throw new RuntimeException(exc);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
