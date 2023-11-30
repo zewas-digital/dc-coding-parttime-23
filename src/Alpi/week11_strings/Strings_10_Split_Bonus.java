eb1e34f1fae208fe736d26e095615b03b6cf9330
Alpi <@>
2023-11-30 17:05:48 +0100

    test

---
+++ src/Alpi/week11_strings/Strings_10_Split_Bonus.java
@@ -0,0 +1,38 @@
+package Alpi.week11_strings;
+
+/*
+Aufgabe: Split -------
+
+1/ Erstelle eine Methode welche den String
+
+"Heute wird ein guter Tag! Heute wird der beste Tag überhaupt! Heute wird ein spitzen super Tag!"
+
+mit Hilfe eines gegebenen Strings zerlegt,
++ welcher als Parameter mitgegeben wird
++ und das String[] zurückgibt.
+
+2/ Füge das String[] mittels eines StringBuilder wieder zusammen
+und füge jedoch nach jedem append auch einen System.lineSeparator() hinzu.
+
+Gib dann den String den du vom StringBuilder erhältst aus.
+(siehe StringBuilder in oliver.week11_strings.Strings_4_Zaehlen)
+
+Teste deine Methode in der main Methode mit einigen Beispielen.
+
+
+Aufruf ---
+
+String[] words = wordSplit(text, "Tag");
+...
+Ausgabe ---
+
+Heute wird ein guter
+! Heute wird der beste
+ überhaupt! Heute wird ein spitzen super
+!
+
+ */
+
+public class Strings_10_Split_Bonus {
+
+}
