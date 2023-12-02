f91b4912c662a1131eaffd948b84830cd0c952b5
mkoenig <@>
2023-12-02 12:09:54 +0100

    week12 Beispiele hinzugefügt

--- src/michael_k/week12/Stream_0_BufferedReader.java
+++ src/michael_k/week12/Stream_0_BufferedReader.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Objects;
 
-public class week12_BufferedReader {
+public class Stream_0_BufferedReader {
 
     public static void main(String[] args) {
         getSimpleText();
