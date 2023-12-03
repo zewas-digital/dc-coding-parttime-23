becc37ee37ffb8b65676cb955aae2b0f2930afd0
mkoenig <@>
2023-12-03 20:13:25 +0100

    week12_Stream_1_Zaehlen fertiggestellt

--- src/michael_k/week12/week12_Stream_1_Zaehlen.java
+++ src/michael_k/week12/week12_Stream_1_Zaehlen.java
@@ -14,6 +14,136 @@ und führe das Zählen über eine Methode aus.
 
  */
 
+import data.Texts;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class week12_Stream_1_Zaehlen {
+
+    public static void main(String[] args) {
+
+        countWords ();
+        countChars ();
+        countSpecificWords ( "und" );
+        countSpecificChar ( 'a' );
+    }
+
+    static void countWords(){
+
+        BufferedReader fileScan = new BufferedReader (
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                Texts.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")
+                        )
+                )
+        );
+
+        String line;
+        int wordCount = 0;
+
+        while ( true ) {
+
+            try {
+                if (( line = fileScan.readLine ( ) ) == null) break;
+
+                String[] words = line.split(" ");
+
+                for (String word : words) {
+                    wordCount++;
+                }
+            } catch (IOException e) {
+                throw new RuntimeException ( e );
+            }
+
+        }
+        System.out.println ("Der Text hat "+wordCount+" Wörter" );
+
+    }
+    static void countChars(){
+        BufferedReader fileScan = new BufferedReader (
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                Texts.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")
+                        )
+                )
+        );
+
+        String line = "";
+        int charCount = 0;
+
+        try {
+            while ( ( line = fileScan.readLine() ) != null){
+
+                    for (int i = 0; i < line.length ( ); i++) {
+                        charCount++;
+                    }
+            }
+            System.out.println ( "Der Text hat "+charCount+" Zeichen" );
+
+        } catch (IOException e) {
+            throw new RuntimeException ( e );
+        }
+    }
+    static void countSpecificWords(String specificWord){
+        BufferedReader fileScan = new BufferedReader (
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                Texts.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")
+                        )
+                )
+        );
+
+        String line;
+        int wordCount = 0;
+
+        while ( true ) {
+
+            try {
+                if (( line = fileScan.readLine ( ) ) == null) break;
+
+                String[] words = line.split(" ");
+
+                for (String word : words) {
+                    if (word.equals ( specificWord )){wordCount++;}
+                }
+            } catch (IOException e) {
+                throw new RuntimeException ( e );
+            }
+
+        }
+        System.out.println ("Der Text hat "+wordCount+" das Wort "+"\""+specificWord+"\""+" enthalten" );
+
+    }
+    static void countSpecificChar(char c){
+        BufferedReader fileScan = new BufferedReader (
+                new InputStreamReader(
+                        Objects.requireNonNull(
+                                Texts.class.getClassLoader().getResourceAsStream("txt/simpleText.txt")
+                        )
+                )
+        );
+
+        String line = "";
+        int charCount = 0;
+
+        try {
+            while ( ( line = fileScan.readLine() ) != null){
+
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == c) {
+                        charCount++;
+                    }
+                }
+            }
+            System.out.println ( "Der Text hat "+charCount+" \""+c+"\""+" Zeichen" );
+
+        } catch (IOException e) {
+            throw new RuntimeException ( e );
+        }
+
+    }
 
 }
