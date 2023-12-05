baee13d91e38a8acda9646597b03dd5e614b8b4f
matth <@>
2023-12-05 18:45:28 +0100

    add new Stream Aufgaben

--- src/Matthias/week12_FileStreams/Stream_2_FileLog.java
+++ src/Matthias/week12_FileStreams/Stream_2_FileLog.java
@@ -1,5 +1,10 @@
 package Matthias.week12_FileStreams;
 
+        import java.io.*;
+        import java.text.SimpleDateFormat;
+        import java.util.Date;
+        import java.util.Scanner;
+
 /*
 Aufgabe: Erstelle eine Klasse für Log-Einträge
 Der Logger schreibt fortlaufend in eine Datei. Es wird nicht überschrieben.
@@ -16,16 +21,51 @@ Ergebnis:
    ...
 */
 public class Stream_2_FileLog {
+    //Einfügen des relativen Links:
+    static String relativeFileLink = "./src/Matthias/week12_FileStreams/iostream.txt";
     public static void main(String[] args) {
+
         // Schreibe Log bei Exception
         int[] numbers = new int[10];
         try {
             numbers[12] = 12;
         }catch (Exception ec){
-            // TODO schreibe log-eintrag mit der geworfenen Exception
+
+            log( 1, "Fehler" );
+            log( 2, "Keine passende Aufgabe gefunden" );
+            log( 3, "Guten Morgen" );
+
         }
     }
     public static void log(int severity, String message) {
+        String TextLogErrorSer1= "Error: ";
+        String TextLogErrorSer2= "Warning: ";
+        String TextLogErrorSer3= "Info: ";
+        switch ( severity ) {
+            case 1:
+                writeToFile(  new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss " ).format( new Date( ) ) + TextLogErrorSer1 +message);
+                break;
+            case 2:
+                writeToFile(   new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss " ).format( new Date( ) )+ TextLogErrorSer2 + message);
+                break;
+            case 3:
+                writeToFile(  new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss " ).format( new Date( ) ) + TextLogErrorSer3 + message);
+                break;
+            }
+        }
 
+    static void writeToFile(String content) {
+        File f = new File(relativeFileLink);
+        try {
+            FileOutputStream fos = new FileOutputStream(f, true);
+            PrintStream ps = new PrintStream(fos);
+
+            ps.println(content);
+            ps.close();
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Datei wurde nicht gefunden.");
+        } finally {
+            System.out.println("+--- wrote file successfully");
+        }
     }
 }
