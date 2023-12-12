fb0f2e555a87de2853869a4f1edccf5b58adac1d
easb2 <12345@>
2023-12-12 20:59:32 +0100

    12.12.23

--- src/saifedine/week13_File/Stream_5_CSVReader.java
+++ src/saifedine/week13_File/Stream_5_CSVReader.java
@@ -10,6 +10,66 @@ ein zweidimensionales Array.
 Gib nun mit Hilfe des String[][], die Daten schön formatiert aus.
  */
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Stream_5_CSVReader {
 
+    static String relativeFileLink = "./resources/csv/sales_100.csv";
+
+    public static void main(String[] args) {
+        // initial: dimensionen bestimmen
+        // --> anzahl zeilen und spalten
+
+        // 1. zeilen aus csv-datei lesen
+        // --> eindimensionales Array
+
+        // 2. datensätze im Array splitten
+        // --> zweidimensionales Array
+
+        // 3. formatierte Ausgabe erzeugen
+
+        printFileInputStream();
+
+        //System.out.println("inputArrayIndex: " + Arrays.toString(printFileInputStream()));
+    }
+
+    static int[][] printFileInputStream() {
+
+        int lineNumber = 0;
+        int countColumns = 0;
+
+        try {
+            FileInputStream fis = new FileInputStream(relativeFileLink);
+            Scanner sc = new Scanner(fis);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                //System.out.println("| " + lineNumber + " " + line);
+
+                lineNumber++;
+
+                countColumns = line.split(",").length;
+            }
+            //System.out.println(lineNumber);
+            //System.out.println(countColumns);
+
+
+            sc.close();
+        } catch (SecurityException e) {
+            System.out.println("Kein Zugriff auf die Datei.");
+        } catch (FileNotFoundException fnf) {
+            System.out.println("Datei wurde nicht gefunden.");
+        }
+
+        //System.out.println(countColumns);
+        //System.out.println(lineNumber);
+
+        int[][] inputArrayIndex = new int[countColumns][lineNumber];
+        System.out.println(Arrays.toString(inputArrayIndex));
+        return inputArrayIndex;
+    }
 }
