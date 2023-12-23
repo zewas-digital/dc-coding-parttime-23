cd38e7a57adfdaeaaa9f032998c3a21eb302a241
matth <@>
2023-12-23 12:08:49 +0100

    add Taskdiscription into example13

--- src/Matthias/week2/example_13_PrintEmptySquare.java
+++ src/Matthias/week2/example_13_PrintEmptySquare.java
@@ -1,5 +1,32 @@
 package Matthias.week2;
+/*
+Erstellen Sie eine Methode welche ein Quader mit variabler Größe und mit angegebenem Zeichen ausgibt. Der Quader soll innerhalb leer sein.
 
+Diese Methode soll mit zwei verschiedenen Aufrufen getestet werden.
+Aufruf:
+printEmptySquare('x', 10);
+
+Ausgabe:
+xxxxxxxxxx
+x        x
+x        x
+x        x
+x        x
+x        x
+x        x
+x        x
+x        x
+xxxxxxxxxx
+
+Aufruf:
+printEmptySquare('A', 3);
+
+Ausgabe:
+AAA
+A A
+AAA
+
+ */
 
 public class example_13_PrintEmptySquare {
 
