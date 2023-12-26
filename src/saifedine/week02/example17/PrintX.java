e7883f4dfe1bc4d0cffbf10331aef14d28c23be6
easb2 <12345@>
2023-12-27 00:01:50 +0100

    26.12.23

--- src/saifedine/week02/example17/PrintX.java
+++ src/saifedine/week02/example17/PrintX.java
@@ -18,4 +18,22 @@ x   x
  */
 
 public class PrintX {
+
+    public static void main(String[] args) {
+
+        char zeichen = 'x';
+        int size = 5;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if ((j == i) || (j == size - 1 - i)){
+                    System.out.print(zeichen);
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
