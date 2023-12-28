f676ffb7fa638824607f368044c9849b7c5d50cb
easb2 <12345@>
2023-12-28 18:05:44 +0100

    27.12.23

--- src/saifedine/week02_weekend1/example22/SumOfDigits.java
+++ src/saifedine/week02_weekend1/example22/SumOfDigits.java
@@ -1,5 +1,5 @@
 /*
-Dieses Programm soll die Ziffernsumme für jede Zahl innerhalb bestimmten Intervalls bilden und jeweils auf der Console ausgeben.
+Dieses Programm soll die Ziffernsumme (z.B. Zahl 11 --> 1+1 = 2) für jede Zahl innerhalb bestimmten Intervalls bilden und jeweils auf der Console ausgeben.
  */
 
 package saifedine.week02_weekend1.example22;
@@ -7,20 +7,23 @@ package saifedine.week02_weekend1.example22;
 public class SumOfDigits {
     public static void main(String[] args) {
 
-        int startWert = 11;
-        int endWert = 15;
+        ziffernSumme(11,15);
+
+    }
+
+    static void ziffernSumme (int startWert, int endWert){
+
         int ziffer1 = 0;
         int ziffer2 = 0;
-        int ziffernsumme = 0;
 
         for (int i = startWert; i <= endWert ; i++) {
 
             System.out.println("i: " + i);
             ziffer1 = (i / 10);
-            System.out.println("ziffer1: " + ziffer1);
+            //System.out.println("ziffer1: " + ziffer1);
 
             ziffer2 = i % 10;
-            System.out.println("ziffer2: " + ziffer2);
+            //System.out.println("ziffer2: " + ziffer2);
 
             System.out.println("ziffernsumme = "  + ziffer1 + " + " +  ziffer2);
 
