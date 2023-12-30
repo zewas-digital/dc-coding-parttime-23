c297fd38d7baa152061d613c9a7832ffc4a9f949
matth <@>
2023-12-30 21:25:54 +0100

    30.12.2023

--- src/Matthias/week2_weekend_Scanner/example_24_PrimeNumbers.java
+++ src/Matthias/week2_weekend_Scanner/example_24_PrimeNumbers.java
@@ -8,7 +8,7 @@ public class example_24_PrimeNumbers {
     public static void main(String[] args) {
         int i = 1; //
         boolean Prim = true; // Prim = true wenn i eine Primzahl ist
-        while (i < 10000) { //
+        while (i < 30) { //
             for (int j = 2; j < (i - 1); j++){
                 if (i % j == 0){
                     Prim = false;
@@ -21,6 +21,5 @@ public class example_24_PrimeNumbers {
             }
             i++;
         }
-
     }
 }
