e98ab2eb0841921a8d30edc340e2da5d0bcbc205
easb2 <12345@>
2024-01-09 21:33:03 +0100

    09.01.24 im Kurs

--- src/saifedine/week14_objects/Object_0_Person.java
+++ src/saifedine/week14_objects/Object_0_Person.java
@@ -4,11 +4,13 @@ public class Object_0_Person {
     public static void main(String[] args) {
 
         Person Roger = new Person();
+
+        // da oben nur die Methode public Person () aufrufen, muss der Name "Roger" und 30 im nachhinein hinzugefügt werden
         Roger.name = "Roger";
         Roger.age = 30;
 
-        int number = 7;
-        String text = "tst";
+        // int number = 7;
+        // String text = "tst";
         char[] charray = {'t','e','s','t'};
 
         String text1 = new String(charray);
@@ -18,7 +20,7 @@ public class Object_0_Person {
         Person Dirk = new Person();
 
         System.out.println(
-                "Neues Objekt für eine Person erstellt: "+ Roger
+                "Neues Objekt für eine Person erstellt: "+ Roger.toString()
                 +"\nDer Name der Person ist "+ Roger.name +" "+ Roger.age
         );
 
