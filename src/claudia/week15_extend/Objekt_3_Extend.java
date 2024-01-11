a9054bf2ea28bb360e57b925fba4234bea673ae9
Claudia <@>
2024-01-11 20:11:24 +0100

    Extend

--- src/claudia/week15_extend/Objekt_3_Extend.java
+++ src/claudia/week15_extend/Objekt_3_Extend.java
@@ -3,7 +3,7 @@ package claudia.week15_extend;
 public class Objekt_3_Extend {
     public static void main(String[] args) {
         // für ein Klassenattribut ist keine Instanz nötig
-        System.out.println("Das Auto hat "+ Auto.wheels +" Räder.");
+        System.out.println("Jedes Auto hat "+ Auto.wheels + " Räder.");
 
         // Objekt / Instanz meinAuto
         Auto meinAuto = new Auto("Mein Auto");
@@ -12,19 +12,19 @@ public class Objekt_3_Extend {
         Einrad meinRad = new Einrad("Mein 1Rad");
 
         // auf ein Instanzattribut kann nur über die mit new erstelle Instanz zugegriffen werden
-        System.out.println("Das Auto hat die Marke "+ meinAuto.marke +".");
+        System.out.println("Das Auto hat die Marke "+ meinAuto.marke + ".");
 
-        System.out.println(meinAuto.getName() +" hat "+ meinAuto.wheels +" Räder.");
-        System.out.println(meinAuto.getName() +" hat einen Literverbrauch von "+ Auto.calcLiterPerKilometer(meinAuto.mileage) +" pro Kilometer.");
+        System.out.println(meinAuto.getName() +" hat "+ meinAuto.wheels + " Räder.");
+        System.out.println(meinAuto.getName() +" hat einen Literverbrauch von "+ Auto.calcLiterPerKilometer(meinAuto.mileage) + " pro Kilometer.");
         System.out.println(meinAuto);
 
-        System.out.println(meinRad.getName() +" hat "+ meinRad.wheels +" Rad.");
+        System.out.println(meinRad.getName() +" hat "+ meinRad.wheels + " Rad.");
 
         // Anonyme Implementierung der abstrakten Klasse Fahrzeug
         Fahrzeug A = new Fahrzeug("A") {
             @Override
             public void drive(int kilometer) {
-                System.out.println("A drives anonymous.");
+                System.out.println("A drives anonymously.");
             }
         };
         A.drive(100);
