80d5fa001caa1a64ce44e37716c69404d8a73772
easb2 <12345@>
2024-01-12 19:48:20 +0100

    12.01.24 im Kurs

---
+++ src/saifedine/week16_weekend3_objects/week15_fahrzeuge/Einrad.java
@@ -0,0 +1,31 @@
+package saifedine.week16_weekend3_objects.week15_fahrzeuge;
+
+public class Einrad extends Fahrzeug {
+    public Einrad(String name) {
+        super(name);
+    }
+
+    @Override
+    public void drive(int kilometer) {
+        System.out.println("Fahre Einrad.");
+        //TODO drive x kilometer...
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
