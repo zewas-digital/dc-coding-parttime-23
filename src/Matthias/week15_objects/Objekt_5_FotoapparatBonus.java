91fc6c9246d63f011faaa1bee285024c3f564948
matth <@>
2024-01-12 19:49:59 +0100

    add New Examples 12.01.2023

--- src/Matthias/week15_objects/Objekt_5_FotoapparatBonus.java
+++ src/Matthias/week15_objects/Objekt_5_FotoapparatBonus.java
@@ -11,4 +11,19 @@ Für die Berechnung speicherplatz pro bild, kann ein wert von 0.3mb pro Megapixe
  */
 
 public class Objekt_5_FotoapparatBonus {
+    public static void main( String[] args ) {
+
+        Fotokamera_5 Fotokamara1= new Fotokamera_5( "Sony","Alpha 4",48 );
+        Objektiv_5 Objektiv=new Objektiv_5( 50 );
+        Speicherkarte_5 Speicherkarte = new Speicherkarte_5( 25 );
+        Fotokamara1.setObjektiv(  Objektiv);
+        Fotokamara1.setSpeicherkarte( Speicherkarte );
+        System.out.println( Fotokamara1 );
+
+        Fotokamara1.objektiv.brennweite=60;
+        System.out.println( Fotokamara1 );
+
+
+
+    }
 }
---
+++ src/Matthias/week15_objects/Objektiv_5.java
@@ -0,0 +1,13 @@
+package Matthias.week15_objects;
+
+public class Objektiv_5 extends Object{
+    public int brennweite;     // Angabe in mm bei Fotoobjektiven
+
+    public Objektiv_5( int brennweite ) {
+        this.brennweite = brennweite;
+    }
+    @Override
+    public String toString() {
+        return "Die Brennweite ist: "+brennweite;
+    }
+}
