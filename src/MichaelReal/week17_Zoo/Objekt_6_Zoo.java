1887d2a3b39dc672d36eb02abbb6aa88244915e9
Michael Real <@>
2024-01-16 19:03:51 +0100

    Zoo

--- src/MichaelReal/week17_Zoo/Objekt_6_Zoo.java
+++ src/MichaelReal/week17_Zoo/Objekt_6_Zoo.java
@@ -21,80 +21,46 @@ Der erwartete Ausdruck sieht folgendermaßen aus:
 
  */
 
-import java.util.ArrayList;
-import java.util.List;
-/*
 public class Objekt_6_Zoo extends Zoo {
-
     public Objekt_6_Zoo(String name, int gruendungsjahr) {
         super(name, gruendungsjahr);
     }
 
-    public static void main(String[] args) {
-        Zoo ersterZoo = new Zoo("Erster Zoo", 2024) {
-        };
-
-        Gehege loewenParadies = new Gehege("Löwen Paradies", "Savanne");
-        Tier tina = new Tier("Tina", "Löwe", "Lammkeule");
-        Tier alfred = new Tier("Alfred", "Löwe", "Grosse Lammkeule");
-
-        Gehege faultiere = new Gehege("Faultiere", "Amazonas");
-        Tier manni = new Tier("Manni", "Faultier", "Beeren");
-        Tier olga = new Tier("Olga", "Faultier", "Rüben");
-
-        Gehege vogelParadies = new Gehege("Vogel Paradies", "Biotop");
-        Tier helga = new Tier("Helga", "Papagei", "Nüsse");
-        Tier peter = new Tier("Peter", "Papagei", "Körner");
-
-        // Gehege zu Zoo hinzufügen
-        ersterZoo.addGehege(loewenParadies);
-        ersterZoo.addGehege(faultiere);
-        ersterZoo.addGehege(vogelParadies);
-
-        // Tiere zu Gehegen hinzufügen
-        ersterZoo.addTierToGehege(tina, loewenParadies);
-        ersterZoo.addTierToGehege(alfred, loewenParadies);
-        ersterZoo.addTierToGehege(manni, faultiere);
-        ersterZoo.addTierToGehege(olga, faultiere);
-        ersterZoo.addTierToGehege(helga, vogelParadies);
-        ersterZoo.addTierToGehege(peter, vogelParadies);
-
-        // Ausgabe des Zoos
-        System.out.println(ersterZoo);
-
-        // Ausgabe eines Geheges
-        System.out.println(loewenParadies);
-
-        // Ausgabe eines Tier
-        System.out.println(tina);
-
-        // Alle Papageien auflisten
-        System.out.println("Alle Papageien:");
-        List<Tier> allePapageien = new ArrayList<>();
-        for (Gehege gehege : ersterZoo.getGehegeList()) {
-            for (Tier tier : gehege.getTiere()) {
-                if (tier.getArt().equals("Papagei")) {
-                    allePapageien.add(tier);
-                }
-            }
-        }
-        for (Tier papagei : allePapageien) {
-            System.out.println(papagei.getName());
-        }
-
-        // Futter ändern
-        tina.setFutter("Rindfleisch");
-
-        // Gehege Habitat ändern
-        loewenParadies.setHabitat("Wüste");
-
-        // Ein bestimmtes Gehege auflisten
-        System.out.println("Tiere im Gehege 'Faultiere':");
-        List<Tier> tiereInFaultiereGehege = ersterZoo.getTiereInGehege(faultiere);
-        if (tiereInFaultiereGehege != null) {
-            for (Tier tier : tiereInFaultiereGehege) {
-                System.out.println(tier.getName());
-            }
-        }
+    @Override
+    public void printAdditionalInfo() {
+        System.out.println("Weitere Informationen über den Objekt_6_Zoo.");
     }
-}*/
+
+    public static void main(String[] args) {
+        Objekt_6_Zoo zoo = new Objekt_6_Zoo("Objekt_6_Zoo", 2022);
+
+        zoo.addGehege("Alpenwiese");
+        zoo.addGehege("Ried");
+        zoo.addGehege("Terrarium (warm)");
+
+        Futter heu = new Futter("Heu");
+        Futter fischfutter = new Futter("Fischfutter");
+        Futter insekten = new Futter("Insekten");
+
+        Tier hase = new Tier("Hase", heu);
+        Tier reh = new Tier("Reh", heu);
+        Tier fisch = new Tier("Fisch", fischfutter);
+        Tier schmetterling = new Tier("Schmetterling", insekten);
+
+        zoo.assignTierToGehege("Alpenwiese", hase);
+        zoo.assignTierToGehege("Alpenwiese", reh);
+        zoo.assignTierToGehege("Ried", fisch);
+        zoo.assignTierToGehege("Terrarium (warm)", schmetterling);
+
+        zoo.printZooStructureWithTiere();
+
+        Futter karotten = new Futter("Karotten");
+        zoo.changeTierFutter("Alpenwiese", "Hase", karotten);
+
+        zoo.printZooStructureWithTiere();
+
+        zoo.removeTierFromGehege("Ried", "Fisch");
+
+        zoo.printZooStructureWithTiere();
+    }
+}
