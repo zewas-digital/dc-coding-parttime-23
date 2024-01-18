1c58224d07655d878151122deaea242e93247732
simonsway <simon.eiler@gmail.com>
2024-01-18 09:48:19 +0100

    Refactor

---
+++ src/Simonsway/Week13/Week18/Objekt_8_FutterBonus.java
@@ -0,0 +1,15 @@
+package Simonsway.Week13.Week18;
+
+/*
+Bonusaufgabe: Tierfutter
+Erweitere dein Zooprogramm mit Futter-Bedarfsanalyse.
+
++ Erstelle eine Klasse für Futter mit einen Namen, Einheit und Einheitspreis.
++ Jedes Tier hat einen Futterbedarf, die beinhaltet das Futter und eine Menge
++ Erstelle eine Statistik, was der Futterbedarf von Zoo ist, und wie viel die Tagesversorgung sich kostet.
+Für diese Aufgabe kann man HashMap gut brauchen.
+ */
+
+public class Objekt_8_FutterBonus {
+
+}
