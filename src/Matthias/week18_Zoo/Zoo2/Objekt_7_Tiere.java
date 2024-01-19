a004c8b6fc4d0d28842f28972077a54095cac8aa
matth <@>
2024-01-19 20:25:57 +0100

    add New Examples 19.01.2023

---
+++ src/Matthias/week18_Zoo/Zoo2/Objekt_7_Tiere.java
@@ -0,0 +1,53 @@
+package Matthias.week18_Zoo.Zoo2;
+
+/*
+Aufgabe: Tiere
+Erweitere dein Zooprogramm mit Tiere.
+
++ Erstelle eine Tier-Klasse mit einem Namen und einer Gattung
+
++ Erweitere die Gehege, um Tiere dynamisch hinzufügen und entfernen zu können.
+
++ Erweitere den Struktur-Ausdruck von Zoo, dass es auch die Tiere ausdrückt.
+
+├── Zoo: Tiergarten Dornbirn, gegründet 2022
+│   ├── Gehege: Alpenwiese
+│       ├── Rijska, Kuh
+│   ├── Gehege: Ried
+│       ├── Garmond, Storch
+│       ├── Hugo, Storh
+│       ├── Idaxis, Storch
+│   ├── Gehege: Terrarium (warm)
+│       ├── (leer)
+ */
+
+public class Objekt_7_Tiere {
+    public static void main( String[] args ) {
+       Zoo zoo1= new Zoo( "Tiergarten Dornbirn", 2022 );
+
+       Gehege gehege1=new Gehege( "Alpenwiese" );
+       Gehege gehege2=new Gehege( "Ried" );
+       Gehege gehege3=new Gehege( "Terrarium (warm)" );
+
+        //ArrayList<Zoo> zoos = new ArrayList<>( );
+
+        zoo1.addGehege( gehege1 );
+        zoo1.addGehege( gehege2 );
+        zoo1.addGehege( gehege3 );
+        zoo1.printFormated();
+        zoo1.removeGehege(gehege1);
+        zoo1.printFormated();
+        zoo1.addGehege( gehege1);
+        zoo1.printFormated();
+        Futterbedarf Menge1=new Futterbedarf( 2 );
+        Futter Futter1 =new Futter( "Fleisch", "kg",0.5 );
+        Futter Futter2 =new Futter( "Fisch", "kg",0.6);
+        Tiere WILD =new Tiere( "Rijska", "Kuh" ,Futter1,Menge1);
+        Tiere VOGEL =new Tiere( "Garmond", "Kuh",Futter2,Menge1 );
+        gehege1.addNewAnimal(  WILD);
+        gehege1.addNewAnimal(  VOGEL);
+        zoo1.printFormated();
+        zoo1.printFutterbedarfsliteZoo();
+    }
+
+}
