536dc7ed0483af5297f3326f45377b0f63fa31f6
finkf <@>
2024-01-20 13:49:34 +0100

    Futter aufgabe fertig
    Pfleger aufgabe begonnen

---
+++ src/florian/week_18_Pflegeraufgabe/Objekt_9_Pfleger.java
@@ -0,0 +1,17 @@
+package florian.week_18_Pflegeraufgabe;
+
+/*
+Aufgabe: Pfleger
+Erweitere dein Zooprogramm um Tierpfleger.
+
+Erstelle eine Klasse Pfleger mit einem Namen und mit einer dynamischen Liste von Gehegen,
+wofür der Pfleger zuständig ist. Erweitere die Klasse Zoo, dass die eine Liste der Pfleger beinhaltet.
+
+Erweitere den Struktur-Ausdruck um die neu eingeführten Pfleger.
+ */
+
+public class Objekt_9_Pfleger {
+    public static void main(String[] args) {
+
+    }
+}
