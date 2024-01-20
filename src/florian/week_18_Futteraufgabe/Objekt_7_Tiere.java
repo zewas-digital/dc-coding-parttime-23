0e7af89be23e02684cab95ac9549aa8f48c88b41
finkf <@>
2024-01-20 13:47:46 +0100

    Zoo Aufgabe fertig
    Zoo Tiere Aufgabe fertig

---
+++ src/florian/week_18_Futteraufgabe/Objekt_7_Tiere.java
@@ -0,0 +1,66 @@
+package florian.week_18_Futteraufgabe;
+
+/*
+Aufgabe: Tiere
+Erweitere dein Zooprogramm mit Tiere.
+
++ Erstelle eine Tier-Klasse mit einem Namen und einer Gattung
+
++ Erweitere die Gehege, um Tiere dynamisch hinzufügen und entfernen zu können.
+
++ Erweitere den Struktur-Ausdruck von Zoo, dass es auch die Tiere ausdrückt.
+
+├── Zoo: Tiergarten Dornbirn, gegründet 2022
+│   ├── Gehege: Alpenwiese
+│       ├── Rijska, Kuh
+│   ├── Gehege: Ried
+│       ├── Garmond, Storh
+│       ├── Hugo, Storh
+│       ├── Idaxis, Storh
+│   ├── Gehege: Terrarium (warm)
+│       ├── (leer)
+ */
+
+public class Objekt_7_Tiere {
+    public static void main(String[] args) {
+
+        String haken = "├──";
+        String haken2 = "│   ├──";
+        String haken3 = "│       ├──";
+
+
+        // Zoo erstellen
+        Zoo tierpark = new Zoo("Doppelmayerzoo", 1980);
+
+        Futterlager futterlager = new Futterlager();
+
+        // Gehege erstellen
+        // Gehege alpen = new Gehege("Alpen");---------------------------
+        // tierpark.gehegeArrayList.add(alpen);
+        tierpark.gehegeArrayList.add(new Gehege("Alpen"));
+        tierpark.gehegeArrayList.add(new Gehege("Savanne"));
+        tierpark.gehegeArrayList.add(new Gehege("Tropen"));
+        //----------------------------------------------------------------
+
+
+        // Tiere erstellen---------------------------------------------------------------------------------------------------------------------
+        tierpark.gehegeArrayList.get(0).tiereImGehege.add(new Tiere("Maxi", "Wildschwein", Futterlager.futterarten.HEU, 5));
+        tierpark.gehegeArrayList.get(0).tiereImGehege.add(new Tiere("Hugo", "Bär", Futterlager.futterarten.FISCH, 15));
+        tierpark.gehegeArrayList.get(1).tiereImGehege.add(new Tiere("Maxiking", "Löwe", Futterlager.futterarten.FISCH, 10));
+        //--------------------------------------------------------------------------------------------------------------------------------------
+
+        //Gehege löschen-----------------------------------------------
+        // tierpark.gehegeArrayList.get(0).tiereImGehege.remove(1);
+        //-------------------------------------------------------------
+        // Ausgabe des Zoos---------------------------------------------------------------------------------------------------
+        System.out.println("Start der Ausgabe");
+        System.out.println(haken + " Zoo: " + tierpark.name + ", gegründet " + tierpark.gruendungsjahr);
+        for (Gehege s : tierpark.gehegeArrayList) {
+            System.out.println(haken2 + " Gehege: " + s.bezeichnung);
+            for (Tiere t : s.tiereImGehege) {
+                System.out.println(haken3 + "Name: " + t.Name + ", Gattung: " + t.Gattung + ", Futter: " + futterlager.lagerliste.get(t.futter).bezeichnung);
+            }
+        }
+    }
+
+}
