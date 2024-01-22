13621267ca92f172ed18d5a2750d18cb402e33e2
easb2 <12345@>
2024-01-22 21:17:19 +0100

    22.01.24 update nach Kurs

--- src/saifedine/week18_Zoo/Objekt_8_FutterBonus.java
+++ src/saifedine/week18_Zoo/Objekt_8_FutterBonus.java
@@ -6,7 +6,7 @@ Erweitere dein Zooprogramm mit Futter-Bedarfsanalyse.
 
 + Erstelle eine Klasse für Futter mit einen Namen, Einheit und Einheitspreis.
 + Jedes Tier hat einen Futterbedarf, die beinhaltet das Futter und eine Menge
-+ Erstelle eine Statistik, was der Futterbedarf von Zoo ist, und wie viel die Tagesversorgung sich kostet.
++ Erstelle eine Statistik, was der Futterbedarf von Zoo ist, und wie viel die Tagesversorgung dich kostet.
 Für diese Aufgabe kann man HashMap gut brauchen.
 
 Beispielausgabe der Statistik:
