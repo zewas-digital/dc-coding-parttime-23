ae6d357dc61d91706ded8b73b8554229bbf1eab7
matth <@>
2024-01-28 10:11:56 +0100

    add New Examples 28.01.2023 2

---
+++ src/Matthias/week19_Zoo/Futter.java
@@ -0,0 +1,32 @@
+package Matthias.week19_Zoo;
+
+public class Futter {
+    public enum Futtersorten{
+        FLEISCH,
+        FISCH,
+        HEU
+    }
+    private  String futterArtikelname;
+
+    private String einheit;
+
+    private double einheitpreis;
+
+    public Futter(String futterArtikelname, String einheit, double einheitspreis){
+        this.futterArtikelname =futterArtikelname;
+        this.einheit=einheit;
+        this.einheitpreis=einheitspreis;
+    }
+
+    public String getFutterArtikelname() {
+        return futterArtikelname;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public double getEinheitpreis() {
+        return einheitpreis;
+    }
+}
