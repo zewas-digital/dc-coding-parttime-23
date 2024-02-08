55da05650bdd952bf597efd904fa0fadc25d027f
easb2 <12345@>
2024-02-08 14:45:02 +0100

    08.02.24 Zoo-Aufgabe

---
+++ src/saifedine/week20_Zoo/V02/Futter.java
@@ -0,0 +1,48 @@
+package saifedine.week20_Zoo.V02;
+
+import java.util.HashMap;
+
+public class Futter {
+
+    // Attribute, enum immer zuerst
+
+    public enum FutterArt {
+        HEU,
+        MAUS,
+        GETREIDE,
+        SALAT
+    }
+
+    public enum FutterEinheit{
+        KG,
+        STK
+    }
+    private FutterArt futterArt;
+    private FutterEinheit futterEinheit;
+    private float futterPreis;
+
+    public static HashMap<FutterArt,Futter> FutterLagerListe = new HashMap<>();
+
+
+    // Konstruktor
+    public Futter(FutterArt FutterArt, FutterEinheit FutterEinheit, float futterPreis) {
+        this.futterArt = FutterArt;
+        this.futterEinheit = FutterEinheit;
+        this.futterPreis = futterPreis;
+        FutterLagerListe.put(FutterArt,this);
+    }
+
+
+    // alle Methoden, wobei toString immer der erste sein soll
+    public String toString() {
+        return this.futterArt + ", " + this.futterEinheit + ", " + this.futterPreis;
+    }
+
+    public float getFutterPreis() {
+        return this.futterPreis;
+    }
+
+    public FutterEinheit getFutterEinheit() {
+        return this.futterEinheit;
+    }
+}
---
+++ src/saifedine/week20_Zoo/V02/Pfleger.java
@@ -0,0 +1,26 @@
+package saifedine.week20_Zoo.V02;
+
+// Objekt_9_Pfleger
+
+import java.util.Vector;
+
+public class Pfleger {
+
+    private String name;
+
+    public Vector<Gehege> gehegeZustaendigkeit;
+
+
+    public Pfleger(String name){
+        this.name = name;
+        this.gehegeZustaendigkeit = new Vector<>();
+    }
+    @Override
+    public String toString(){
+        return "Pfleger: " + this.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
