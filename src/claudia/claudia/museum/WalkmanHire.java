633a92a116e69fb63e2bb109b64e97ce07a9a480
Claudia <@>
2024-02-20 18:11:11 +0100

    Museum

--- src/claudia/claudia/museum/WalkmanHire.java
+++ src/claudia/claudia/museum/WalkmanHire.java
@@ -1,9 +1,26 @@
 package claudia.claudia.museum;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WalkmanHire {
     static int pool;
     static int helpers;
     public static void main(String[] args) {
+
+
+        Timer meinTimer = new Timer();
+        meinTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("\n\nDas Museum schließt jetzt, auf Wiedersehen!");
+                meinTimer.cancel();
+                System.exit(0); //beendet die main-Methode
+            }
+        }, 30000); //30 Sekunden
+
+
+
         if (args.length >= 1)
             pool = Integer.parseInt(args[0]);
         else pool = 50;
