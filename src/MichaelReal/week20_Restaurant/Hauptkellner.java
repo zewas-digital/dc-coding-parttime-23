bc967952d1cf1788e1dfa5398b8c2d5c220cda2e
Michael Real <@>
2024-02-22 21:49:43 +0100

    restaurant

---
+++ src/MichaelReal/week20_Restaurant/Hauptkellner.java
@@ -0,0 +1,14 @@
+package MichaelReal.week20_Restaurant;
+
+public class Hauptkellner extends Kellner {
+    public Hauptkellner(String name) {
+        super(name);
+    }
+
+    // Spezifische Methoden für den Hauptkellner
+
+    public void tischZuweisen(Tisch tisch, Kellner kellner) {
+        // Logik zur Zuweisung eines Tisches zu einem Kellner
+    }
+
+}
