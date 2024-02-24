93cbb0191fee77ed49d64583736960b28ad6eea3
Michael Real <@>
2024-02-24 15:45:50 +0100

    geometrie

--- src/MichaelReal/week20_Geometrie/Geometry.java
+++ src/MichaelReal/week20_Geometrie/Geometry.java
@@ -1,10 +1,8 @@
 package MichaelReal.week20_Geometrie;
 
 public abstract class Geometry implements Calculation {
-    @Override
     public abstract double getArea();
 
-    @Override
     public abstract double getCircumference();
 }
 
