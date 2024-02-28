590f1ffec866a9994d1c01c393b2b346bbaa75bc
easb2 <12345@>
2024-02-28 21:35:15 +0100

    28.02.24 Versionsablgeich

---
+++ src/saifedine/week21/oliver_Zoo_Simulation/Futter.java
@@ -0,0 +1,25 @@
+package saifedine.week21.oliver_Zoo_Simulation;
+
+public class Futter {
+    private String name;
+    private String einheit;
+    private int preisProEinheit;
+
+    public Futter(String name, String einheit, int preisProEinheit) {
+        this.name = name;
+        this.einheit = einheit;
+        this.preisProEinheit = preisProEinheit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEinheit() {
+        return einheit;
+    }
+
+    public int getPreisProEinheit() {
+        return preisProEinheit;
+    }
+}
