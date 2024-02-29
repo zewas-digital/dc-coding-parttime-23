92a938173a15b206d3ef06c0244341ecd48f1cd7
Michael Real <@>
2024-02-29 17:58:46 +0100

    carsimulation

---
+++ src/MichaelReal/week22_Carsimulation/PetrolCar.java
@@ -0,0 +1,13 @@
+package MichaelReal.week22_Carsimulation;
+
+public class PetrolCar extends Car {
+    public PetrolCar(String hersteller, String modell, int kW, double tankinhalt, double gewicht, double verbrauch) {
+        super(hersteller, modell, kW, tankinhalt, Antriebsart.BENZIN, gewicht, verbrauch);
+    }
+
+    @Override
+    public void simulateDrive() {
+        System.out.println("Das Benzinauto " + modell + " von " + hersteller + " beginnt die Fahrt.");
+        // Hier könntest du Logik implementieren, um die Fahrt zu simulieren.
+    }
+}
