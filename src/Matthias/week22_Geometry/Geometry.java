64c930a4cd0842d1554588511b648aa247fba6bc
matth <@>
2024-02-29 17:15:40 +0100

    Update 29.02.2024

---
+++ src/Matthias/week22_Geometry/Geometry.java
@@ -0,0 +1,11 @@
+package Matthias.week22_Geometry;
+
+public abstract class Geometry implements Calculation {
+    double area;
+    double circumference;
+
+    // Konstruktor für Abstrakte Klasse
+    public Geometry() {
+
+    }
+}
