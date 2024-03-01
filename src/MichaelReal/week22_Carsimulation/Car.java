276c721929ef120b67ab3eb6d1d3426db55b0624
Michael Real <@>
2024-03-01 18:04:46 +0100

    carsimulation

--- src/MichaelReal/week22_Carsimulation/Car.java
+++ src/MichaelReal/week22_Carsimulation/Car.java
@@ -7,49 +7,26 @@ public abstract class Car {
         BENZIN, DIESEL, GAS, STROM
     }
 
-    // Attribute der Car Klasse
-    protected String hersteller;
-    protected String modell;
-    protected int kW; // Leistung
-    protected double tankinhalt; // Aktueller Tankinhalt in Litern
-    protected double maximaleTankkapazitaet; // Maximale Tankkapazität in Litern
-    protected Antriebsart antrieb;
-    protected double gewicht;
-    protected double verbrauch; // Kraftstoffverbrauch (Kilometer pro Liter)
+        protected String hersteller;
+        protected String modell;
+        protected int kW; // Leistung
+        protected Antriebsart antrieb;
+        protected double gewicht;
+        protected double verbrauch; // Abhängig von der Antriebsart, interpretiert in Unterklassen
 
-    public Car(String hersteller, String modell, int kW, double tankinhalt, Antriebsart antrieb, double gewicht, double verbrauch, double maximaleTankkapazitaet) {
-        this.hersteller = hersteller;
-        this.modell = modell;
-        this.kW = kW;
-        this.tankinhalt = tankinhalt;
-        this.antrieb = antrieb;
-        this.gewicht = gewicht;
-        this.verbrauch = verbrauch;
-        this.maximaleTankkapazitaet = maximaleTankkapazitaet;
+        public Car(String hersteller, String modell, int kW, Antriebsart antrieb, double gewicht, double verbrauch) {
+            this.hersteller = hersteller;
+            this.modell = modell;
+            this.kW = kW;
+            this.antrieb = antrieb;
+            this.gewicht = gewicht;
+            this.verbrauch = verbrauch;
+        }
+
+        public abstract int drive(int kilometer);
+
+        // Getter-Methoden
+        public String getHersteller() { return hersteller; }
+        public String getModell() { return modell; }
     }
 
-    public abstract void simulateDrive();
-
-    public int drive(int kilometer) {
-        int maxDistanz = (int) (tankinhalt * verbrauch);
-        if (kilometer <= maxDistanz) {
-            tankinhalt -= kilometer / verbrauch;
-            return kilometer;
-        } else {
-            tankinhalt = 0; // Tank ist leer
-            return maxDistanz;
-        }
-    }
-
-    public void refuel(double liter) {
-        if (liter < 0) {
-            System.out.println("Die Kraftstoffmenge kann nicht negativ sein.");
-            return;
-        }
-        this.tankinhalt += liter;
-        if (this.tankinhalt > this.maximaleTankkapazitaet) {
-            this.tankinhalt = this.maximaleTankkapazitaet;
-        }
-        System.out.println("Das Auto wurde aufgetankt. Aktueller Tankinhalt: " + this.tankinhalt + " Liter");
-    }
-}
