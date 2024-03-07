e839f9011f9c55c701dc62b0dc881a02ae83fce7
easb2 <12345@>
2024-03-07 15:40:39 +0100

    07.03.24 - File-Abgleich Bank vom 05.03.24

---
+++ src/saifedine/week23/Bank/Bank.java
@@ -0,0 +1,24 @@
+package saifedine.week23.Bank;
+
+
+/*
+
+Ein Bankinstitut hat verschiedene Schalter. Ein Kunde geht in ein Bankinstitut um sein Bankgeschäft zu verrichten. Zuerst geht der Kunde ins Institut und geht zum nächsten freien Schalter.
+Wenn der Kunde den Schalter wieder verlässt, wird der Schalter zu einer Wahrscheinlichkeit von 20% für eine Pause geschlossen (die Pause eines Schalters endet nach dem 3 weitere Kunden die Bank betreten und verlassen haben).
+Hat ein Schalter kein Geld mehr so muss dieser wieder aufgefüllt werden und der Schalter ist für die Dauer eines Kunden geschlossen.
+
+        * Das Bankgeschäft eines Kunden ist entweder eine Einzahlung bzw. eine Auszahlung.
+        * Kann ein Schalter eine gewisse Geldmenge nicht bedienen geht der Kunde zum nächsten Schalter und der Ursprüngliche Schalter wird aufgefüllt.
+        * Es soll nachvollzogen werden können welcher Schalter welche Kunden bedient hat und was für ein Betrag eingezahlt oder ausgezahlt wurde (ACHTUNG keine Dictionary).
+
+        Erstelle ein UML Diagramm für dein Banksystem (Dia)
+        Erstelle die notwendingen Klassen und Methoden.
+        Erstelle nun eine Bank mit einigen Schaltern und erstelle weiters einige Kunden.
+        Simuliere nun das Bankgeschäft. Die Kunden gehen der reihe nach in die Bank. Am Ende der Simulation soll für jeden Schalter eine Statistik ausgegeben werden (welche Kunden waren dort, welche Beträge wurden verarbeitet).
+        Erstelle Unit Tests für deine Banksimulation
+
+       */
+
+
+public class Bank {
+}
